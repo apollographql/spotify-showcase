3f4a55c9e98b9369db15590be5b7e23a37b45ba8
Jerel Miller <jerelmiller@gmail.com>
2022-12-12 16:46:41 -0700

    Auto import resolvers

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,5 +1,21 @@
-import Query from './Query';
+import fs from 'fs';
+import path from 'path';
 
-export default {
-  Query,
-};
+const importResolver = (filename: string) =>
+  require(path.resolve(__dirname, filename)).default;
+
+const resolverName = (filename: string) =>
+  path.basename(filename, path.extname(filename));
+
+const resolvers = fs
+  .readdirSync(__dirname)
+  .filter((filename) => filename !== path.basename(__filename))
+  .reduce(
+    (resolvers, filename) => ({
+      ...resolvers,
+      [resolverName(filename)]: importResolver(filename),
+    }),
+    {}
+  );
+
+export default resolvers;
