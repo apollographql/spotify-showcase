18e5e12826359df4315d4fd81b4a9443748cd715
Jerel Miller <jerelmiller@gmail.com>
2022-12-13 10:55:18 -0700

    Add codegen

---
+++ server/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: './src/schema.graphql',
+  generates: {
+    './src/resolvers/types.ts': {
+      config: {
+        useIndexSignature: true,
+      },
+      plugins: ['typescript', 'typescript-resolvers'],
+    },
+  },
+};
+
+export default config;
