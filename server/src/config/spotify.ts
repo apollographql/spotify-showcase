9ca496659983e948b61726f76d003c8cce66b40a
Jerel Miller <jerelmiller@gmail.com>
2022-12-12 22:32:26 -0700

    Update spotify config

---
+++ server/src/config/spotify.ts
@@ -0,0 +1,16 @@
+import invariant from 'ts-invariant';
+
+const readEnv = (key: string) => {
+  const value = process.env[key];
+
+  invariant(value, `Ensure process.env.${key} is defined`);
+
+  return value;
+};
+
+export default {
+  clientId: readEnv('SPOTIFY_CLIENT_ID'),
+  clientSecret: readEnv('SPOTIFY_CLIENT_SECRET'),
+  redirectURI: 'http://localhost:4000',
+  clientURI: 'http://localhost:3000',
+};
