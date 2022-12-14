f27286719ac54020099e74f25dd85dbb5d90e757
Jerel Miller <jerelmiller@gmail.com>
2022-12-13 22:09:40 -0700

    Add history buttons

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,4 +1,7 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode, useState } from 'react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import Flex from '../Flex';
 import styles from './Layout.module.scss';
 
 interface HeaderProps {
@@ -6,7 +9,50 @@ interface HeaderProps {
 }
 
 const Header = ({ children }: HeaderProps) => {
-  return <header className={styles.header}>{children}</header>;
+  const [historyStack] = useState([]);
+  const [backStack] = useState([]);
+  const navigate = useNavigate();
+
+  return (
+    <header className={styles.header}>
+      <Flex gap="1rem">
+        <NavButton
+          disabled={historyStack.length === 0}
+          aria-label="Go back"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <ChevronLeft size={20} />
+        </NavButton>
+        <NavButton
+          aria-label="Go forward"
+          disabled={backStack.length === 0}
+          onClick={() => navigate(1)}
+        >
+          <ChevronRight size={20} />
+        </NavButton>
+      </Flex>
+      {children}
+    </header>
+  );
+};
+
+type NativeButtonProps = Pick<
+  ComponentPropsWithoutRef<'button'>,
+  'disabled' | 'onClick' | 'aria-label'
+>;
+
+type NavButtonProps = NativeButtonProps & {
+  children: ReactNode;
+};
+
+const NavButton = ({ children, ...props }: NavButtonProps) => {
+  return (
+    <button {...props} className={styles.headerNavButton}>
+      {children}
+    </button>
+  );
 };
 
 export default Header;
