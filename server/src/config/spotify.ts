b755f315f7028c812e56010940e21c3cf7b9f8d0
Jerel Miller <jerelmiller@gmail.com>
2022-12-13 23:43:28 -0700

    Lazy load spotify config to allow app to start

--- server/src/config/spotify.ts
+++ server/src/config/spotify.ts
@@ -3,14 +3,23 @@ import invariant from 'ts-invariant';
 const readEnv = (key: string) => {
   const value = process.env[key];
 
-  invariant(value, `Ensure process.env.${key} is defined`);
+  invariant(
+    value,
+    `\`process.env.${key}\` must be defined. To get started, visit the home page at http://localhost:3000.`
+  );
 
   return value;
 };
 
-export default {
-  clientId: readEnv('SPOTIFY_CLIENT_ID'),
-  clientSecret: readEnv('SPOTIFY_CLIENT_SECRET'),
+const config = {
+  get clientId() {
+    return readEnv('SPOTIFY_CLIENT_ID');
+  },
+  get clientSecret() {
+    return readEnv('SPOTIFY_CLIENT_SECRET');
+  },
   redirectURI: 'http://localhost:4000/oauth/finalize',
   clientURI: 'http://localhost:3000',
 };
+
+export default config;
