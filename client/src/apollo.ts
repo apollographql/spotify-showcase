6995206148ec7c9fa84dab78f0a035d85ec460de
Jerel Miller <jerelmiller@gmail.com>
2022-12-13 18:17:37 -0700

    Remove token when unauthorized

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,13 +1,31 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const httpLink = createHttpLink({
+  uri: `${process.env.REACT_APP_SERVER_HOST}/graphql`,
+  headers: {
+    get 'x-api-token'() {
+      return localStorage.getItem('token');
+    },
+  },
+});
+
+const removeTokenLink = onError(({ graphQLErrors }) => {
+  graphQLErrors?.forEach((error) => {
+    if (error.extensions.code === 'UNAUTHENTICATED') {
+      localStorage.removeItem('token');
+    }
+  });
+});
+
+const link = ApolloLink.from([removeTokenLink, httpLink]);
 
 export default new ApolloClient({
   cache: new InMemoryCache(),
-  link: createHttpLink({
-    uri: `${process.env.REACT_APP_SERVER_HOST}/graphql`,
-    headers: {
-      get 'x-api-token'() {
-        return localStorage.getItem('token');
-      },
-    },
-  }),
+  link,
 });
