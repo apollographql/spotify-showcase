2448d6aa7af84a6f290022c11067ab331c115a19
Jerel Miller <jerelmiller@gmail.com>
2022-12-14 12:12:23 -0700

    Move header to main component

--- client/src/components/Layout/Main.tsx
+++ client/src/components/Layout/Main.tsx
@@ -1,12 +1,29 @@
 import { ReactNode } from 'react';
+import Button from '../Button';
+import Header from './Header';
+import useIsLoggedIn from '../../hooks/useIsLoggedIn';
 import styles from './Main.module.scss';
+import { LOGIN_URL } from '../../constants';
 
 interface MainProps {
   children: ReactNode;
 }
 
 const Main = ({ children }: MainProps) => {
-  return <main className={styles.main}>{children}</main>;
+  const isLoggedIn = useIsLoggedIn();
+
+  return (
+    <main className={styles.main}>
+      <Header>
+        {!isLoggedIn && (
+          <Button as="a" size="sm" variant="primary" href={LOGIN_URL}>
+            Log in
+          </Button>
+        )}
+      </Header>
+      <article className={styles.mainContent}>{children}</article>
+    </main>
+  );
 };
 
 export default Main;
--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,26 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Button from '../components/Button';
 import Layout from '../components/Layout';
-import useIsLoggedIn from '../hooks/useIsLoggedIn';
-import { LOGIN_URL } from '../constants';
 
 const Root = () => {
-  const isLoggedIn = useIsLoggedIn();
-
   return (
     <Layout>
       <Layout.Sidebar />
       <Layout.Main>
-        <Layout.Header>
-          {!isLoggedIn && (
-            <Button as="a" size="sm" variant="primary" href={LOGIN_URL}>
-              Log in
-            </Button>
-          )}
-        </Layout.Header>
-        <article>
-          <Outlet />
-        </article>
+        <Outlet />
       </Layout.Main>
     </Layout>
   );
