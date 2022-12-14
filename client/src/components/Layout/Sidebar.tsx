3d2f927c98f29d92b777af4734d7d465dc46fd33
Jerel Miller <jerelmiller@gmail.com>
2022-12-14 12:55:47 -0700

    Get work done in place to show logged in content

--- client/src/components/Layout/Sidebar.tsx
+++ client/src/components/Layout/Sidebar.tsx
@@ -1,28 +1,27 @@
-import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import { Library, Home, Search, Heart } from 'lucide-react';
+import cx from 'classnames';
 import Flex from '../Flex';
 import ApolloLogo from '../ApolloLogo';
 import GradientIcon from '../GradientIcon';
 import SpotifyLogo from '../SpotifyLogo';
 import NavLink from './NavLink';
+import useIsLoggedIn from '../../hooks/useIsLoggedIn';
 import styles from './Sidebar.module.scss';
 
-interface SidebarProps {
-  children?: ReactNode;
-}
+const Sidebar = () => {
+  const isLoggedIn = useIsLoggedIn();
 
-const Sidebar = ({ children }: SidebarProps) => {
   return (
     <aside className={styles.sidebar}>
-      <nav>
+      <nav className={styles.sidebarNav}>
         <div className={styles.sidebarLogo}>
           <Flex as={Link} to="/" inline alignItems="end" direction="column">
             <ApolloLogo size="225px" />
             <SpotifyLogo size="100px" />
           </Flex>
         </div>
-        <ul className={styles.sidebarLinks}>
+        <ul className={styles.sidebarNavSection}>
           <NavLink icon={<Home />} to="/">
             Home
           </NavLink>
@@ -33,7 +32,7 @@ const Sidebar = ({ children }: SidebarProps) => {
             Your Library
           </NavLink>
         </ul>
-        <ul className={styles.sidebarLinks}>
+        <ul className={styles.sidebarNavSection}>
           <NavLink
             icon={
               <GradientIcon
@@ -46,9 +45,25 @@ const Sidebar = ({ children }: SidebarProps) => {
             Liked Songs
           </NavLink>
         </ul>
-        {children}
+        {isLoggedIn && (
+          <>
+            <hr className={styles.sidebarDivider} />
+            <Playlists />
+          </>
+        )}
       </nav>
     </aside>
+  );
+};
+
+const Playlists = () => {
+  return (
+    <ul className={cx(styles.sidebarNavSection, styles.sidebarPlaylists)}>
+      <NavLink to="/playlists/1">Daily Mix 1</NavLink>
+      <NavLink to="/playlists/2">Daily Mix 2</NavLink>
+      <NavLink to="/playlists/3">Daily Mix 3</NavLink>
+      <NavLink to="/playlists/4">Daily Mix 4</NavLink>
+    </ul>
   );
 };
 
