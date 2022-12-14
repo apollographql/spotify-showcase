f16f02eb25df9b46defb2274e9e770f948e896af
Jerel Miller <jerelmiller@gmail.com>
2022-12-14 11:25:07 -0700

    Shift around where layout is rendered

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,7 +1,29 @@
 import { Outlet } from 'react-router-dom';
+import Button from '../components/Button';
+import Layout from '../components/Layout';
+import useIsLoggedIn from '../hooks/useIsLoggedIn';
+import { LOGIN_URL } from '../constants';
 
 const Root = () => {
-  return <Outlet />;
+  const isLoggedIn = useIsLoggedIn();
+
+  return (
+    <Layout>
+      <Layout.Sidebar />
+      <Layout.Main>
+        <Layout.Header>
+          {!isLoggedIn && (
+            <Button as="a" size="sm" variant="primary" href={LOGIN_URL}>
+              Log in
+            </Button>
+          )}
+        </Layout.Header>
+        <article>
+          <Outlet />
+        </article>
+      </Layout.Main>
+    </Layout>
+  );
 };
 
 export default Root;
