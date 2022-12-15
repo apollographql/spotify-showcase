4441c480329ce5b9293ea8df173960bebba14200
Jerel Miller <jerelmiller@gmail.com>
2022-12-14 18:17:59 -0700

    Get dynamic colors working

--- client/src/hooks/useSetBackgroundColorFromImage.ts
+++ client/src/hooks/useSetBackgroundColorFromImage.ts
@@ -2,6 +2,15 @@ import { useEffect } from 'react';
 import useBackgroundColor from './useBackgroundColor';
 import Vibrant from 'node-vibrant';
 
+const PREFERRED_SWATCHES = [
+  'Vibrant',
+  'LightVibrant',
+  'DarkVibrant',
+  'Muted',
+  'LightMuted',
+  'DarkMuted',
+];
+
 const useSetBackgroundColorFromImage = (src: string | null | undefined) => {
   const [, setBackgroundColor] = useBackgroundColor();
 
@@ -13,38 +22,23 @@ const useSetBackgroundColorFromImage = (src: string | null | undefined) => {
     const img = new Image();
 
     img.crossOrigin = 'anonymous';
-    img.onload = () => {
-      console.log({ Vibrant, default: Vibrant.DefaultOpts });
-      // Vibrant.from(img).getPalette((err, palette) => {
-      //   console.log({ err, palette });
-      // });
+    img.onload = async () => {
+      const result = await Vibrant.from(img).getPalette();
+
+      const name = PREFERRED_SWATCHES.find((name) => result[name]);
+
+      if (name) {
+        const swatch = result[name]!;
+
+        setBackgroundColor(swatch.hex);
+      }
     };
     img.src = src;
 
     return () => {
       img.onload = null;
     };
-
-    // const result = Vibrant.from(src);
-
-    // console.log(result);
-    // const img = new Image();
-
-    // if (src) {
-    //   // img.crossOrigin = true;
-    //   img.onload = () => {
-    //     // const colorThief = new ColorThief();
-    //     // const color = rgb(...colorThief.getColor(img));
-    //     // setColor(shade(0.3, color));
-    //   };
-    //   img.src = src;
-    // }
-
-    // return () => {
-    //   img.onload = null;
-    // };
-    //
-  }, [src]);
+  }, [src, setBackgroundColor]);
 };
 
 export default useSetBackgroundColorFromImage;
