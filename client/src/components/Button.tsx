7d7d621a878decab4c413d20fa374ed6d9b7910d
Jerel Miller <jerelmiller@gmail.com>
2022-12-16 15:49:24 -0700

    Allow Button to take a ref

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,37 +1,53 @@
-import { ReactNode, ComponentPropsWithoutRef, ElementType } from 'react';
+import { ReactNode, ElementType, forwardRef, ReactElement } from 'react';
 import cx from 'classnames';
 import styles from './Button.module.scss';
+import {
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from '../utils/types';
 
 export type ButtonProps<TButtonElement extends ElementType = 'button'> =
-  ComponentPropsWithoutRef<TButtonElement> & {
-    as?: TButtonElement;
-    children: ReactNode;
-    className?: string;
-    size?: 'xs' | 'sm' | 'md';
-    variant: 'primary' | 'ghost' | 'hollow';
-  };
+  PolymorphicComponentPropsWithRef<
+    TButtonElement,
+    {
+      children: ReactNode;
+      className?: string;
+      size?: 'xs' | 'sm' | 'md';
+      variant: 'primary' | 'ghost' | 'hollow';
+    }
+  >;
 
-const Button = <TButtonElement extends ElementType = 'button'>({
-  as,
-  children,
-  className,
-  size = 'md',
-  variant,
-  ...props
-}: ButtonProps<TButtonElement>) => {
-  const ButtonElement = as || 'button';
+type ButtonCompoment = <TButtonElement extends ElementType = 'button'>(
+  props: ButtonProps<TButtonElement>
+) => ReactElement | null;
 
-  return (
-    <ButtonElement
-      {...props}
-      className={cx(styles.button, className, {
-        [styles[size ?? '']]: size,
-        [styles[variant ?? '']]: variant,
-      })}
-    >
-      {children}
-    </ButtonElement>
-  );
-};
+const Button: ButtonCompoment = forwardRef(
+  <TButtonElement extends ElementType = 'button'>(
+    {
+      as,
+      children,
+      className,
+      size = 'md',
+      variant,
+      ...props
+    }: ButtonProps<TButtonElement>,
+    ref?: PolymorphicRef<TButtonElement>
+  ) => {
+    const ButtonElement = as || 'button';
+
+    return (
+      <ButtonElement
+        {...props}
+        ref={ref}
+        className={cx(styles.button, className, {
+          [styles[size ?? '']]: size,
+          [styles[variant ?? '']]: variant,
+        })}
+      >
+        {children}
+      </ButtonElement>
+    );
+  }
+);
 
 export default Button;
