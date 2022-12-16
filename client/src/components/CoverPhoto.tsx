3888df42854f0cf13ad1407a2336065afe446667
Jerel Miller <jerelmiller@gmail.com>
2022-12-16 11:27:16 -0700

    Allow cover photo to accept size

--- client/src/components/CoverPhoto.tsx
+++ client/src/components/CoverPhoto.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, cloneElement } from 'react';
+import { ReactElement, cloneElement, CSSProperties } from 'react';
 import cx from 'classnames';
 import LazyImage from './LazyImage';
 import styles from './CoverPhoto.module.scss';
@@ -6,16 +6,28 @@ import styles from './CoverPhoto.module.scss';
 interface CoverPhotoProps {
   className?: string;
   src: string;
+  size?: string;
   fallback: ReactElement<{ className?: string }>;
 }
 
-const CoverPhoto = ({ className, fallback, src }: CoverPhotoProps) => {
-  return src ? (
-    <LazyImage className={cx(styles.playlistImage, className)} src={src} />
-  ) : (
-    cloneElement(fallback, {
-      className: cx(styles.coverPhoto, fallback.props.className, className),
-    })
+interface StyleProps extends CSSProperties {
+  '--cover-photo--size': CSSProperties['width'];
+}
+
+const CoverPhoto = ({ className, fallback, size, src }: CoverPhotoProps) => {
+  return (
+    <div
+      className={cx(styles.container, className)}
+      style={{ '--cover-photo--size': size } as StyleProps}
+    >
+      {src ? (
+        <LazyImage className={cx(styles.playlistImage)} src={src} />
+      ) : (
+        cloneElement(fallback, {
+          className: cx(styles.coverPhoto, fallback.props.className),
+        })
+      )}
+    </div>
   );
 };
 
