05bf290aff4cf71b06fb1e23f1806b7bea5db435
Jerel Miller <jerelmiller@gmail.com>
2022-12-16 15:49:48 -0700

    Add a useStableCallback hook

---
+++ client/src/hooks/useStableCallback.ts
@@ -0,0 +1,17 @@
+import { useEffect, useRef, useCallback } from 'react';
+
+const useStableCallback = <TArgs extends any[], TReturn>(
+  callback: (...args: TArgs) => TReturn
+) => {
+  const ref = useRef(callback);
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  return useCallback((...args: TArgs) => {
+    return ref.current(...args);
+  }, []);
+};
+
+export default useStableCallback;
