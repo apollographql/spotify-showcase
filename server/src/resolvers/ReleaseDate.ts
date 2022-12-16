d81eb12584da82cdc51f42e5e39272e3fdf07f4b
Jerel Miller <jerelmiller@gmail.com>
2022-12-15 21:10:49 -0700

    Fix date field on release date type

--- server/src/resolvers/ReleaseDate.ts
+++ server/src/resolvers/ReleaseDate.ts
@@ -2,6 +2,7 @@ import { ReleaseDateResolvers } from './types';
 import { prop } from './helpers';
 
 const resolvers: ReleaseDateResolvers = {
+  date: prop('release_date'),
   precision: prop('release_date_precision'),
 };
 
