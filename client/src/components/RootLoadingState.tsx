21e8f160f3f58afa42c066bd5a61bd3a0d1ca4a9
Jerel Miller <jerelmiller@gmail.com>
2022-12-20 02:47:42 -0700

    Create root loading state with skeletons

---
+++ client/src/components/RootLoadingState.tsx
@@ -0,0 +1,36 @@
+import cx from 'classnames';
+import Layout from './Layout';
+import Skeleton from './Skeleton';
+import headerStyles from './Layout/Header.module.scss';
+import { randomBetween } from '../utils/common';
+import { range } from '../utils/lists';
+import styles from './RootLoadingState.module.scss';
+import Flex from './Flex';
+
+const RootLoadingState = () => {
+  const skeletons = range(0, randomBetween(10, 15));
+
+  return (
+    <Layout>
+      <Layout.Sidebar>
+        <Layout.Sidebar.Section>
+          {skeletons.map((num) => (
+            <li className={styles.skeleton__playlist}>
+              <Skeleton.Text key={num} width={`${randomBetween(40, 60)}%`} />
+            </li>
+          ))}
+        </Layout.Sidebar.Section>
+      </Layout.Sidebar>
+      <Layout.Main>
+        <header className={cx(headerStyles.header, styles.header)}>
+          <Flex gap="0.5rem" alignItems="center">
+            <Skeleton.Avatar size="2rem" />
+            <Skeleton.Text width="3rem" />
+          </Flex>
+        </header>
+      </Layout.Main>
+    </Layout>
+  );
+};
+
+export default RootLoadingState;
