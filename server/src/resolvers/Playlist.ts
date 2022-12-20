c31af84099e31794abc52c5d51f8d0e884cce1e8
Jerel Miller <jerelmiller@gmail.com>
2022-12-20 13:30:52 -0700

    Filter out playlist tracks with no track property

--- server/src/resolvers/Playlist.ts
+++ server/src/resolvers/Playlist.ts
@@ -3,8 +3,21 @@ import { prop } from './helpers';
 
 const resolvers: PlaylistResolvers = {
   externalUrls: prop('external_urls'),
-  tracks: (playlist, { limit, offset }, { dataSources }) => {
-    return dataSources.spotify.playlistTracks(playlist.id, { limit, offset });
+  tracks: async (playlist, { limit, offset }, { dataSources }) => {
+    const playlistTracks = await dataSources.spotify.playlistTracks(
+      playlist.id,
+      {
+        limit,
+        offset,
+      }
+    );
+
+    return {
+      ...playlistTracks,
+      // Sometimes the API returns items in a playlist with no track, so we
+      // filter them out.
+      items: playlistTracks.items.filter((item) => item.track),
+    };
   },
 };
 
