c464747c3526c9637d27a2b1a2f7e6a4306f53da
Jerel Miller <jerelmiller@gmail.com>
2022-12-19 22:55:46 -0700

    Use outer suspense boundary on first load

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,16 +1,29 @@
-import { Suspense } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Layout from '../components/Layout';
 
 const Root = () => {
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    setIsInitialized(true);
+  }, []);
+
+  // Allow children in the route to suspend using the outer Suspense boundary
+  // and only use the suspense boundary on the main area when the initial set of
+  // data is fully loaded.
+  const children = isInitialized ? (
+    <Suspense fallback="Loading...">
+      <Outlet />
+    </Suspense>
+  ) : (
+    <Outlet />
+  );
+
   return (
     <Layout>
       <Layout.Sidebar />
-      <Layout.Main>
-        <Suspense fallback="Loading...">
-          <Outlet />
-        </Suspense>
-      </Layout.Main>
+      <Layout.Main>{children}</Layout.Main>
     </Layout>
   );
 };
