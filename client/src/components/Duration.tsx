3c418eef3f690b6880809b8d7fa44338b7adec8a
Jerel Miller <jerelmiller@gmail.com>
2022-12-21 00:39:15 -0700

    Only pad minutes in duration if there are hours to display

--- client/src/components/Duration.tsx
+++ client/src/components/Duration.tsx
@@ -6,15 +6,17 @@ const Duration = ({ durationMs }: DurationProps) => {
   const seconds = Math.floor(durationMs / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
-  const segments = [minutes % 60, seconds % 60];
+
+  const segments = [
+    String(minutes % 60).padStart(hours > 0 ? 2 : 1, '0'),
+    String(seconds % 60).padStart(2, '0'),
+  ];
 
   if (hours > 0) {
-    segments.unshift(hours);
+    segments.unshift(String(hours).padStart(2, '0'));
   }
 
-  return (
-    <>{segments.map((num) => num.toString().padStart(2, '0')).join(':')}</>
-  );
+  return <>{segments.join(':')}</>;
 };
 
 export default Duration;
