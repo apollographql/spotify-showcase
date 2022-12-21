ff6a668188170403de2f7ef2165174705ab788cd
Jerel Miller <jerelmiller@gmail.com>
2022-12-21 13:10:23 -0700

    Add box shadow on page cover photo

--- client/src/components/Page/Header.tsx
+++ client/src/components/Page/Header.tsx
@@ -1,4 +1,6 @@
 import { cloneElement, ReactElement } from 'react';
+import cx from 'classnames';
+import styles from './Page.module.scss';
 
 import Details from './Details';
 import HeaderContainer from './HeaderContainer';
@@ -7,7 +9,7 @@ import MediaType from './MediaType';
 import Title from './Title';
 
 interface HeaderProps {
-  coverPhoto: ReactElement<{ size: string }>;
+  coverPhoto: ReactElement<{ className: string; size: string }>;
   details: ReactElement[];
   mediaType?: string;
   title: string;
@@ -16,7 +18,10 @@ interface HeaderProps {
 const Header = ({ coverPhoto, details, mediaType, title }: HeaderProps) => {
   return (
     <HeaderContainer>
-      {cloneElement(coverPhoto, { size: '250px' })}
+      {cloneElement(coverPhoto, {
+        size: '250px',
+        className: cx(coverPhoto.props.className, styles.page__coverPhoto),
+      })}
       <HeaderDetails>
         {mediaType && <MediaType mediaType={mediaType} />}
         <Title>{title}</Title>
