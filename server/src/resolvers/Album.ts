98a8bd83cdbdb3bb8271d52819d77b812004b087
Jerel Miller <jerelmiller@gmail.com>
2022-12-20 18:31:51 -0700

    Ensure externalUrls resolves properly

--- server/src/resolvers/Album.ts
+++ server/src/resolvers/Album.ts
@@ -3,6 +3,7 @@ import { itself, prop } from './helpers';
 
 const resolvers: AlbumResolvers = {
   albumType: prop('album_type'),
+  externalUrls: prop('external_urls'),
   releaseDate: itself(),
   totalTracks: prop('total_tracks'),
 };
