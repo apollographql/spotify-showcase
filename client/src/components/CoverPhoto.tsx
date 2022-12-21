b16b3f809955acec1b35f3bd43691eaabb41e5be
Jerel Miller <jerelmiller@gmail.com>
2022-12-21 12:44:46 -0700

    Allow cover photo to have different shapes

--- client/src/components/CoverPhoto.tsx
+++ client/src/components/CoverPhoto.tsx
@@ -12,16 +12,26 @@ export interface CoverPhotoProps {
   image: Image | null | undefined;
   size?: string;
   fallback: ReactElement<{ className?: string }>;
+  shape?: 'square' | 'circle';
 }
 
 interface StyleProps extends CSSProperties {
   '--cover-photo--size': CSSProperties['width'];
 }
 
-const CoverPhoto = ({ className, fallback, size, image }: CoverPhotoProps) => {
+const CoverPhoto = ({
+  className,
+  fallback,
+  size,
+  image,
+  shape = 'square',
+}: CoverPhotoProps) => {
   return (
     <div
-      className={cx(styles.container, className)}
+      className={cx(styles.container, className, {
+        [styles.square]: shape === 'square',
+        [styles.circle]: shape === 'circle',
+      })}
       style={{ '--cover-photo--size': size } as StyleProps}
     >
       {image ? (
