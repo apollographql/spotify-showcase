3195306f7736d267c21b752ec820da04c89a706d
Jerel Miller <jerelmiller@gmail.com>
2022-12-20 21:15:53 -0700

    Create album track title cell

--- client/src/components/AlbumTrackTitleCell.tsx
+++ client/src/components/AlbumTrackTitleCell.tsx
@@ -1,9 +1,10 @@
-import { Fragment } from 'react';
 import { gql } from '@apollo/client';
 import { AlbumTrackTitleCell_track as Track } from '../types/api';
+import cx from 'classnames';
 import CommaSeparatedList from './CommaSeparatedList';
 import EntityLink from './EntityLink';
 import Flex from './Flex';
+import typography from '../styles/typography.module.scss';
 
 interface AlbumTrackTitleCellProps {
   track: Track;
@@ -12,10 +13,17 @@ interface AlbumTrackTitleCellProps {
 const AlbumTrackTitleCell = ({ track }: AlbumTrackTitleCellProps) => {
   return (
     <Flex direction="column" gap="0.5">
-      {track.name}
+      <span className={typography.text__body}>{track.name}</span>
       <CommaSeparatedList>
         {track.artists.map((artist) => (
-          <EntityLink key={artist.id} entity={artist}>
+          <EntityLink
+            key={artist.id}
+            className={cx(
+              typography.text__muted,
+              typography.text__muted__interactive
+            )}
+            entity={artist}
+          >
             {artist.name}
           </EntityLink>
         ))}
