252d4f929a22798d29e850f6b8a888b628097b0c
Jerel Miller <jerelmiller@gmail.com>
2022-12-21 01:24:59 -0700

    Create a component to render a media tile

---
+++ client/src/components/MediaTile.tsx
@@ -0,0 +1,47 @@
+import { Music } from 'lucide-react';
+import { ReactNode } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import CoverPhoto from './CoverPhoto';
+import PlaceholderCoverPhoto from './PlaceholderCoverPhoto';
+import Text from './Text';
+import styles from './MediaTile.module.scss';
+import Flex from './Flex';
+
+interface MediaTileProps {
+  coverPhotoSrc: string;
+  title: string;
+  description: ReactNode;
+  to: LinkProps['to'];
+}
+
+const MediaTile = ({
+  coverPhotoSrc,
+  description,
+  title,
+  to,
+}: MediaTileProps) => {
+  return (
+    <Flex
+      as={Link}
+      direction="column"
+      gap="1rem"
+      to={to}
+      className={styles.mediaTile}
+    >
+      <CoverPhoto
+        src={coverPhotoSrc}
+        fallback={<PlaceholderCoverPhoto icon={Music} />}
+      />
+      <Flex direction="column">
+        <Text wrap={false} overflow="ellipsis" weight="bold" title={title}>
+          {title}
+        </Text>
+        <Text color="muted" size="sm">
+          {description}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default MediaTile;
