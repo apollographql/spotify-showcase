54c053c1fb6538d25c2125b3421337d95882b0d0
Jerel Miller <jerelmiller@gmail.com>
2023-01-03 21:06:15 -0700

    Allow Duration to handle multiple formats

--- client/src/components/Duration.tsx
+++ client/src/components/Duration.tsx
@@ -1,22 +1,52 @@
 interface DurationProps {
   durationMs: number;
+  format?: keyof typeof FORMAT;
 }
 
-const Duration = ({ durationMs }: DurationProps) => {
+interface TimeSegments {
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const FORMAT = {
+  SHORT: 'SHORT',
+  LONG: 'LONG',
+} as const;
+
+const Duration = ({ durationMs, format = FORMAT.SHORT }: DurationProps) => {
   const seconds = Math.floor(durationMs / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
+  const formatter = formatters[format];
 
-  const segments = [
-    String(minutes % 60).padStart(hours > 0 ? 2 : 1, '0'),
-    String(seconds % 60).padStart(2, '0'),
-  ];
+  return <>{formatter({ seconds, minutes, hours })}</>;
+};
 
-  if (hours > 0) {
-    segments.unshift(String(hours).padStart(2, '0'));
-  }
+Duration.FORMAT = FORMAT;
 
-  return <>{segments.join(':')}</>;
+const formatters = {
+  [FORMAT.LONG]: ({ seconds, minutes, hours }: TimeSegments) => {
+    const segments = [`${minutes % 60} min`, `${seconds % 60} sec`];
+
+    if (hours > 0) {
+      segments.unshift(`${hours} hr`);
+    }
+
+    return segments.join(' ');
+  },
+  [FORMAT.SHORT]: ({ seconds, minutes, hours }: TimeSegments) => {
+    const segments = [
+      String(minutes % 60).padStart(hours > 0 ? 2 : 1, '0'),
+      String(seconds % 60).padStart(2, '0'),
+    ];
+
+    if (hours > 0) {
+      segments.unshift(String(hours).padStart(2, '0'));
+    }
+
+    return segments.join(':');
+  },
 };
 
 export default Duration;
