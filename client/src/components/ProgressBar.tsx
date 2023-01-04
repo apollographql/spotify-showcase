36eea9a32dccffeb027cd2ca33c413ff91ea2184
Jerel Miller <jerelmiller@gmail.com>
2023-01-03 21:50:06 -0700

    Create a progress bar component

---
+++ client/src/components/ProgressBar.tsx
@@ -0,0 +1,33 @@
+import cx from 'classnames';
+import * as Progress from '@radix-ui/react-progress';
+import styles from './ProgressBar.module.scss';
+import { CSSProperties } from 'react';
+
+interface ProgressBarProps {
+  className?: string;
+  max: number;
+  value: number;
+  width?: CSSProperties['width'];
+}
+
+interface StyleProps extends CSSProperties {
+  '--progress-bar--width': CSSProperties['width'];
+}
+
+const ProgressBar = ({ className, max, value, width }: ProgressBarProps) => {
+  return (
+    <Progress.Root
+      className={cx(styles.progressBar, className)}
+      max={max}
+      value={value}
+      style={{ '--progress-bar--width': width } as StyleProps}
+    >
+      <Progress.Indicator
+        className={styles.progressIndicator}
+        style={{ transform: `translateX(-${(value / max) * 100}%)` }}
+      />
+    </Progress.Root>
+  );
+};
+
+export default ProgressBar;
