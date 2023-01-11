6740c2857d952a72a5ea3de8433378ecd259923b
Jerel Miller <jerelmiller@gmail.com>
2023-01-11 02:17:31 -0700

    More robust playback state by detecting only changed requested fields before publishing

--- server/src/resolvers/Subscription.ts
+++ server/src/resolvers/Subscription.ts
@@ -1,17 +1,33 @@
 import { SubscriptionResolvers } from './types';
 import { pollPlaybackState } from '../utils/spotify';
+import camelize from 'camelize';
 import { TOPICS } from '../constants';
+import { map, distinctUntilChanged } from 'rxjs';
+import { equal } from '@wry/equality';
+import { getSelectionsMap, FieldSelections } from '../utils/graphql';
+import { isPlainObject } from '../utils/common';
 
 const resolvers: SubscriptionResolvers = {
   playbackStateChanged: {
-    subscribe: async (_, __, { pubsub, dataSources }) => {
-      const subscription = pollPlaybackState(dataSources.spotify).subscribe(
-        (playbackState) => {
+    subscribe: async (_, __, { pubsub, dataSources }, info) => {
+      const selections = getSelectionsMap(
+        info.fieldNodes[0].selectionSet,
+        info.fragments
+      );
+
+      const subscription = pollPlaybackState(dataSources.spotify)
+        .pipe(
+          map(
+            (playbackState) =>
+              playbackState && pickSelectedFields(playbackState, selections)
+          ),
+          distinctUntilChanged((prev, curr) => equal(prev, curr))
+        )
+        .subscribe((playbackState) => {
           pubsub.publish(TOPICS.PLAYBACK_STATE_CHANGED, {
             playbackStateChanged: playbackState,
           });
-        }
-      );
+        });
 
       pubsub.subscribe(TOPICS.DISCONNECT, () => {
         subscription.unsubscribe();
@@ -20,6 +36,54 @@ const resolvers: SubscriptionResolvers = {
       return pubsub.asyncIterator(TOPICS.PLAYBACK_STATE_CHANGED) as any;
     },
   },
+};
+
+const ALWAYS_KEPT_FIELDS = ['type'];
+
+// Return a partial representation of the object based on the selection set.
+// This allows us to more easily detect if the playback has changed only for the
+// fields that we care about
+const pickSelectedFields = <T extends object>(
+  object: T,
+  selections: FieldSelections
+): Partial<T> => {
+  const map = Object.entries(object).reduce((map, [key, value]) => {
+    const camelizedKey = camelize(key);
+
+    if (ALWAYS_KEPT_FIELDS.includes(key)) {
+      return map.set(key, value);
+    }
+
+    if (!selections.has(camelizedKey)) {
+      return map;
+    }
+
+    if (isPlainObject(value)) {
+      return map.set(
+        key,
+        pickSelectedFields(
+          value,
+          selections.get(camelizedKey) as FieldSelections
+        )
+      );
+    }
+
+    if (Array.isArray(value)) {
+      return map.set(
+        key,
+        value.map((item) =>
+          pickSelectedFields(
+            item,
+            selections.get(camelizedKey) as FieldSelections
+          )
+        )
+      );
+    }
+
+    return map.set(key, value);
+  }, new Map());
+
+  return Object.fromEntries(map.entries());
 };
 
 export default resolvers;
--- server/src/utils/graphql.ts
+++ server/src/utils/graphql.ts
@@ -1,10 +1,12 @@
 import {
   FieldNode,
+  FragmentDefinitionNode,
   GraphQLResolveInfo,
   Kind,
   SelectionSetNode,
   SelectionNode,
 } from 'graphql';
+import { ObjMap } from 'graphql/jsutils/ObjMap';
 
 export const selectsField = (path: string[], info: GraphQLResolveInfo) => {
   return info.fieldNodes.some((fieldNode) =>
@@ -45,4 +47,39 @@ const selectsFieldInNode = (
       return selectsFieldInNode(path, fragment.selectionSet, info);
     }
   }
+};
+
+export type FieldSelections = Map<string, boolean | FieldSelections>;
+
+export const getSelectionsMap = (
+  selectionSet: SelectionSetNode | undefined,
+  fragments: ObjMap<FragmentDefinitionNode>,
+  map = new Map<string, boolean | FieldSelections>()
+): FieldSelections => {
+  if (!selectionSet) {
+    return map;
+  }
+
+  return selectionSet.selections.reduce((map, node) => {
+    switch (node.kind) {
+      case Kind.FIELD:
+        if (node.selectionSet) {
+          return map.set(
+            node.name.value,
+            getSelectionsMap(node.selectionSet, fragments, new Map())
+          );
+        }
+
+        return map.set(node.name.value, true);
+      case Kind.FRAGMENT_SPREAD: {
+        const fragment = fragments[node.name.value];
+
+        return getSelectionsMap(fragment.selectionSet, fragments, map);
+      }
+      case Kind.INLINE_FRAGMENT:
+        return getSelectionsMap(node.selectionSet, fragments, map);
+      default:
+        return map;
+    }
+  }, map);
 };
