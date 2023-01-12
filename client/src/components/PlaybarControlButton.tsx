24681e082b6b5831de7058585e183225bc6aa8ca
Jerel Miller <jerelmiller@gmail.com>
2023-01-12 13:04:11 -0700

    Add active styles and consistent sizing for playback control button

--- client/src/components/PlaybarControlButton.tsx
+++ client/src/components/PlaybarControlButton.tsx
@@ -1,21 +1,24 @@
 import { ReactElement } from 'react';
 import { LucideProps } from 'lucide-react';
+import cx from 'classnames';
 import styles from './PlaybarControlButton.module.scss';
 
 interface PlaybarControlButtonProps {
+  active?: boolean;
   disallowed: boolean;
   icon: ReactElement<LucideProps>;
   onClick?: () => void;
 }
 
 const PlaybarControlButton = ({
+  active,
   disallowed,
   icon,
   onClick,
 }: PlaybarControlButtonProps) => {
   return (
     <button
-      className={styles.playbarControlButton}
+      className={cx(styles.playbarControlButton, { [styles.active]: active })}
       disabled={disallowed}
       onClick={onClick}
     >
