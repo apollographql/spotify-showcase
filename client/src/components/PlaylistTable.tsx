bf938d33d305c0a0c424dc1800c347134333d552
Jerel Miller <jerelmiller@gmail.com>
2023-01-13 16:36:20 -0700

    Move columns inside component

--- client/src/components/PlaylistTable.tsx
+++ client/src/components/PlaylistTable.tsx
@@ -20,13 +20,6 @@ interface PlaylistTableProps {
 
 type PlaylistTrackEdge = NonNullable<Get<Playlist, 'tracks.edges[0]'>>;
 
-interface CellMeta {
-  containsAllTracks: boolean;
-  containsAllEpisodes: boolean;
-  containsAddedDate: boolean;
-  playlist: Playlist;
-}
-
 const columnHelper = createColumnHelper<PlaylistTrackEdge>();
 
 const parentOf = (playlistItem: PlaylistTrackEdge['node']) => {
@@ -37,119 +30,113 @@ const parentOf = (playlistItem: PlaylistTrackEdge['node']) => {
   return playlistItem.album;
 };
 
-const columns = [
-  columnHelper.accessor('node', {
-    id: 'type',
-    header: '#',
-    cell: (info) => {
-      const playlistTrack = info.getValue();
-      const meta = info.table.options.meta! as CellMeta;
-      const { index } = info.row;
-      const { containsAllTracks, playlist } = meta!;
-
-      if (playlistTrack.__typename === 'Track') {
-        return (
-          <TrackNumberCell
-            context={playlist}
-            track={playlistTrack}
-            position={index}
-            preferIcon={!containsAllTracks}
-          />
-        );
-      }
-
-      return <Podcast size="1rem" />;
-    },
-    meta: {
-      headerAlign: 'right',
-      shrink: true,
-    },
-  }),
-  columnHelper.accessor('node', {
-    id: 'title',
-    header: 'Title',
-    cell: (info) => <PlaylistTitleCell playlistTrack={info.getValue()} />,
-  }),
-  columnHelper.accessor(({ node }) => parentOf(node), {
-    id: 'albumOrPodcast',
-    header: ({ table }) => {
-      const { meta } = table.options;
-
-      switch (true) {
-        case meta?.containsAllTracks:
-          return 'Album';
-        case meta?.containsAllEpisodes:
-          return 'Podcast';
-        default:
-          return 'Album or podcast';
-      }
-    },
-    cell: (info) => {
-      const parent = info.getValue();
-
-      return <EntityLink entity={parent}>{parent.name}</EntityLink>;
-    },
-  }),
-  columnHelper.accessor(
-    ({ node }) => {
-      return node.__typename === 'Episode' ? node.releaseDate : null;
-    },
-    {
-      id: 'releaseDate',
-      header: 'Release date',
-      cell: (info) => {
-        const releaseDate = info.getValue();
-
-        return releaseDate && <ReleaseDate releaseDate={releaseDate} />;
-      },
-    }
-  ),
-  columnHelper.accessor('addedAt', {
-    header: 'Date added',
-    cell: (info) => {
-      const date = info.getValue();
-
-      return date && <DateTime date={date} format={DateTime.FORMAT.timeAgo} />;
-    },
-    meta: {
-      wrap: false,
-    },
-  }),
-  columnHelper.accessor('node.durationMs', {
-    header: () => <Clock size="1rem" />,
-    cell: (info) => <Duration durationMs={info.getValue()} />,
-    meta: {
-      shrink: true,
-      headerAlign: 'right',
-    },
-  }),
-];
-
 const PlaylistTable = ({ className, playlist }: PlaylistTableProps) => {
   const [resumePlayback] = useResumePlaybackMutation();
 
-  const meta = useMemo(
-    () => ({
-      playlist,
-      containsAllTracks: playlist.tracks.edges.every(
-        ({ node }) => node.__typename === 'Track'
-      ),
-      containsAllEpisodes: playlist.tracks.edges.every(
-        ({ node }) => node.__typename === 'Episode'
-      ),
-      containsAddedDate: playlist.tracks.edges.some((edge) =>
-        Boolean(edge.addedAt)
-      ),
-    }),
-    [playlist]
+  const containsAllTracks = playlist.tracks.edges.every(
+    ({ node }) => node.__typename === 'Track'
   );
+  const containsAllEpisodes = playlist.tracks.edges.every(
+    ({ node }) => node.__typename === 'Episode'
+  );
+
+  const containsAddedDate = playlist.tracks.edges.some((edge) =>
+    Boolean(edge.addedAt)
+  );
+
+  const columns = useMemo(() => {
+    return [
+      columnHelper.accessor('node', {
+        id: 'type',
+        header: '#',
+        cell: (info) => {
+          const playlistTrack = info.getValue();
+          const { index } = info.row;
+
+          if (playlistTrack.__typename === 'Track') {
+            return (
+              <TrackNumberCell
+                context={playlist}
+                track={playlistTrack}
+                position={index}
+                preferIcon={!containsAllTracks}
+              />
+            );
+          }
+
+          return <Podcast size="1rem" />;
+        },
+        meta: {
+          headerAlign: 'right',
+          shrink: true,
+        },
+      }),
+      columnHelper.accessor('node', {
+        id: 'title',
+        header: 'Title',
+        cell: (info) => <PlaylistTitleCell playlistTrack={info.getValue()} />,
+      }),
+      columnHelper.accessor(({ node }) => parentOf(node), {
+        id: 'albumOrPodcast',
+        header: () => {
+          switch (true) {
+            case containsAllTracks:
+              return 'Album';
+            case containsAllEpisodes:
+              return 'Podcast';
+            default:
+              return 'Album or podcast';
+          }
+        },
+        cell: (info) => {
+          const parent = info.getValue();
+
+          return <EntityLink entity={parent}>{parent.name}</EntityLink>;
+        },
+      }),
+      columnHelper.accessor(
+        ({ node }) => {
+          return node.__typename === 'Episode' ? node.releaseDate : null;
+        },
+        {
+          id: 'releaseDate',
+          header: 'Release date',
+          cell: (info) => {
+            const releaseDate = info.getValue();
+
+            return releaseDate && <ReleaseDate releaseDate={releaseDate} />;
+          },
+        }
+      ),
+      columnHelper.accessor('addedAt', {
+        header: 'Date added',
+        cell: (info) => {
+          const date = info.getValue();
+
+          return (
+            date && <DateTime date={date} format={DateTime.FORMAT.timeAgo} />
+          );
+        },
+        meta: {
+          wrap: false,
+        },
+      }),
+      columnHelper.accessor('node.durationMs', {
+        header: () => <Clock size="1rem" />,
+        cell: (info) => <Duration durationMs={info.getValue()} />,
+        meta: {
+          shrink: true,
+          headerAlign: 'right',
+        },
+      }),
+    ];
+  }, [playlist, containsAllEpisodes, containsAllTracks]);
 
   return (
     <Table
       className={className}
       data={playlist.tracks.edges}
       columns={columns}
-      meta={meta}
       onDoubleClickRow={(row) => {
         const { node } = row.original;
 
@@ -158,8 +145,8 @@ const PlaylistTable = ({ className, playlist }: PlaylistTableProps) => {
         });
       }}
       visibility={{
-        addedAt: meta.containsAddedDate,
-        releaseDate: !meta.containsAllTracks,
+        addedAt: containsAddedDate,
+        releaseDate: !containsAllTracks,
       }}
     />
   );
