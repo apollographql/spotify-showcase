d69b7808773c1ef0bc98fb84daa28282b3a7be55
Jerel Miller <jerelmiller@gmail.com>
2023-01-16 14:27:37 -0700

    Use eslint config init

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 };
