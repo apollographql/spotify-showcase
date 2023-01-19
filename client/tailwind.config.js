5df84f82b69b02b1dc0a8cbaae475200ea9afd7b
Jerel Miller <jerelmiller@gmail.com>
2023-01-19 16:43:32 -0700

    Add some tailwind styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,40 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'CircularSp',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Kaku Gothic Pro',
+        'Meiryo',
+        'MS Gothic',
+        'sans-serif',
+      ],
+    },
+    colors: {
+      black: {
+        base: '#121212',
+        pure: '#000',
+      },
+      green: {
+        DEFAULT: 'hsl(140, 63%, 44%)',
+        light: 'hsl(140, 63%, 54%)',
+      },
+      offwhite: '#b3b3b3',
+      surface: {
+        DEFAULT: '#282828',
+        lowContrast: '#181818',
+      },
+      transparent: 'transparent',
+      white: '#fff',
+    },
+    extend: {
+      fontSize: {
+        xxs: ['0.625rem', '0.875rem'],
+      },
+    },
   },
   plugins: [],
 };
