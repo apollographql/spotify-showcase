a1b677a011f0b14dce1601aa19399201b777c16e
Jerel Miller <jerelmiller@gmail.com>
2023-01-19 16:43:59 -0700

    Convert Button to tailwind

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, ElementType, forwardRef, ReactElement } from 'react';
 import cx from 'classnames';
-import styles from './Button.module.scss';
 import {
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
@@ -40,10 +39,24 @@ const Button: ButtonCompoment = forwardRef(function Button<
     <ButtonElement
       {...props}
       ref={ref}
-      className={cx(styles.button, className, {
-        [styles[size ?? '']]: size,
-        [styles[variant ?? '']]: variant,
-      })}
+      data-size={size}
+      className={cx(
+        className,
+        'inline-flex scale-100 transform-gpu cursor-pointer items-center rounded-3xl border-2 border-solid text-xl uppercase transition-all duration-150 ease-in-out [backface-visibility:hidden]',
+        'hover:scale-[1.05] hover:no-underline aria-expanded:scale-[1.05]',
+        'focus:outline-0',
+        {
+          ['text-xxs py-2 px-5 tracking-wide']: size === 'xs',
+          ['py-3 px-8 text-xs tracking-wide']: size === 'sm',
+          ['py-3 px-12 text-base']: size === 'md',
+          ['bg-green border-green hover:bg-green-light hover:border-green-light text-white']:
+            variant === 'primary',
+          ['border-offwhite bg-transparent text-white hover:border-white']:
+            variant === 'hollow',
+          ['hover:background-black-pure/10 border-transparent bg-transparent text-white']:
+            variant === 'ghost',
+        }
+      )}
     >
       {children}
     </ButtonElement>
