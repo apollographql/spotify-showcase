038d5bdb82c2357d3c667062104652c62c0cd6a4
Jerel Miller <jerelmiller@gmail.com>
2023-01-20 02:38:19 -0700

    Allow text to be customized in context entity link

--- client/src/components/ContextMenuAction/CopyLinkToEntity.tsx
+++ client/src/components/ContextMenuAction/CopyLinkToEntity.tsx
@@ -1,8 +1,10 @@
+import { ReactNode } from 'react';
 import { copyToClipboard } from '../../utils/copyToClipboard';
 import { Entity, getEntityPathname } from '../../utils/navigation';
 import ContextMenu from '../ContextMenu/ContextMenu';
 
 interface CopyLinkToEntityProps {
+  children?: ReactNode;
   entity: Entity;
 }
 
@@ -16,7 +18,10 @@ const LABELS: Record<Entity['__typename'], string> = {
   User: 'Copy link to profile',
 };
 
-const CopyLinkToEntity = ({ entity }: CopyLinkToEntityProps) => {
+const CopyLinkToEntity = ({
+  entity,
+  children = LABELS[entity.__typename],
+}: CopyLinkToEntityProps) => {
   return (
     <ContextMenu.Action
       onSelect={() => {
@@ -25,7 +30,7 @@ const CopyLinkToEntity = ({ entity }: CopyLinkToEntityProps) => {
         );
       }}
     >
-      {LABELS[entity.__typename]}
+      {children}
     </ContextMenu.Action>
   );
 };
