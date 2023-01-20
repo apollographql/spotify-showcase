47de72838b2528cd3843fbf745344fc2c9839583
Jerel Miller <jerelmiller@gmail.com>
2023-01-20 00:46:49 -0700

    Fix how context is determined when opening in desktop app

--- client/src/components/ContextMenuAction/OpenDesktopApp.tsx
+++ client/src/components/ContextMenuAction/OpenDesktopApp.tsx
@@ -1,29 +1,19 @@
-import { gql } from '@apollo/client';
-import { OpenDesktopApp_playbackState as PlaybackState } from '../../types/api';
-import usePlaybackState from '../../hooks/usePlaybackState';
 import ContextMenu from '../ContextMenu';
 
 interface OpenDesktopAppProps {
+  context?: Context;
   uri: string;
 }
 
-const PLAYBACK_STATE_FRAGMENT = gql`
-  fragment OpenDesktopApp_playbackState on PlaybackState {
-    context {
-      uri
-    }
-  }
-`;
+interface Context {
+  uri: string;
+}
 
-const OpenDesktopApp = ({ uri }: OpenDesktopAppProps) => {
-  const playbackState = usePlaybackState<PlaybackState>({
-    fragment: PLAYBACK_STATE_FRAGMENT,
-  });
-
+const OpenDesktopApp = ({ uri, context }: OpenDesktopAppProps) => {
   const url = new URL(uri);
 
-  if (playbackState?.context) {
-    url.searchParams.set('context', playbackState.context.uri);
+  if (context) {
+    url.searchParams.set('context', context.uri);
   }
 
   return (
