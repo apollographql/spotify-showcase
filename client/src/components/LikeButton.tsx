0ee44bc91d6e2d60431e7ab94dd08df9776336e9
Jerel Miller <jerelmiller@gmail.com>
2023-01-23 10:03:01 -0700

    Add tooltip to like button

--- client/src/components/LikeButton.tsx
+++ client/src/components/LikeButton.tsx
@@ -1,6 +1,7 @@
 import { Heart, LucideProps } from 'lucide-react';
 import cx from 'classnames';
 import styles from './LikeButton.module.scss';
+import Tooltip from './Tooltip';
 
 export interface LikeButtonProps {
   className?: string;
@@ -9,19 +10,20 @@ export interface LikeButtonProps {
   size?: LucideProps['size'];
 }
 
-const LikeButton = ({
-  className,
-  liked: isLiked,
-  onClick,
-  size,
-}: LikeButtonProps) => {
+const LikeButton = ({ className, liked, onClick, size }: LikeButtonProps) => {
   return (
-    <button
-      className={cx(styles.likeButton, className, { [styles.liked]: isLiked })}
-      onClick={onClick}
+    <Tooltip
+      content={liked ? 'Remove from Your Library' : 'Save to Your Library'}
     >
-      <Heart fill={isLiked ? 'currentColor' : 'transparent'} size={size} />
-    </button>
+      <button
+        className={cx(styles.likeButton, className, {
+          [styles.liked]: liked,
+        })}
+        onClick={onClick}
+      >
+        <Heart fill={liked ? 'currentColor' : 'transparent'} size={size} />
+      </button>
+    </Tooltip>
   );
 };
 
