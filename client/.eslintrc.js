ea048edd2b18667b73ea38abbc2b1c2f445808cf
Jerel Miller <jerelmiller@gmail.com>
2023-01-22 20:40:28 -0700

    Turn off display name lint error

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,7 +18,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'react/display-name': 'off',
+  },
   settings: {
     react: {
       version: 'detect',
