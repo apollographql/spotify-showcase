1989b57865a7dbb21f642a39bcb1e1c9cf3ba0fa
Jerel Miller <jerelmiller@gmail.com>
2023-01-25 02:20:04 -0700

    Disable key fields for Developer

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -69,6 +69,9 @@ export default new ApolloClient({
           tracksContains: libraryContains(),
         },
       },
+      Developer: {
+        keyFields: [],
+      },
       Followers: {
         keyFields: false,
       },
