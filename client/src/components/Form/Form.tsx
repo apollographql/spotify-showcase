fa702fa909c42602d3c551dc260689d988c72f36
Jerel Miller <jerelmiller@gmail.com>
2023-01-25 01:18:16 -0700

    Move TextField over to Form.TextField

---
+++ client/src/components/Form/Form.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import { Formik, FormikConfig, FormikValues } from 'formik';
+import TextField from './TextField';
+
+type ForwardedFormikProps<TValues> = Pick<
+  FormikConfig<TValues>,
+  'initialValues' | 'validate'
+>;
+
+interface FormProps<TValues, TSubmittedValues extends TValues = TValues>
+  extends ForwardedFormikProps<TValues> {
+  children: ReactNode;
+  onSubmit: (values: TSubmittedValues) => void | Promise<unknown>;
+}
+
+function Form<
+  TValues extends FormikValues,
+  TSubmittedValues extends TValues = TValues
+>({ children, onSubmit, ...props }: FormProps<TValues, TSubmittedValues>) {
+  return (
+    <Formik
+      {...props}
+      onSubmit={(values) => onSubmit(values as TSubmittedValues)}
+      enableReinitialize={true}
+      validateOnBlur={false}
+      validateOnChange={false}
+    >
+      {({ handleSubmit }) => {
+        return <form onSubmit={handleSubmit}>{children}</form>;
+      }}
+    </Formik>
+  );
+}
+
+Form.TextField = TextField;
+
+export default Form;
---
+++ client/src/components/Form/TextField.tsx
@@ -0,0 +1,127 @@
+import { forwardRef, ChangeEvent, ComponentPropsWithoutRef } from 'react';
+import cx from 'classnames';
+import { useField } from 'formik';
+import { clamp, omit } from 'lodash';
+import { parseFloatValue, parseIntValue } from '../../utils/form';
+
+type ForwardedInputProps = Omit<
+  ComponentPropsWithoutRef<'input'>,
+  'onChange' | 'value'
+>;
+
+// Props allowed with specific input types
+interface InputTypeSpecificProps {
+  float: {
+    scale?: number;
+  };
+}
+
+type TextFieldTypeProps = {
+  [Type in TextFieldType]: Type extends keyof InputTypeSpecificProps
+    ? { type?: Type } & InputTypeSpecificProps[Type]
+    : { type?: Type };
+}[TextFieldType];
+
+type TextFieldType = 'text' | 'int' | 'float';
+
+// Allow numbers to have partially typed negative number or empty string
+type PartialNumberValue = '' | '-';
+
+type TextFieldValueByType = {
+  text: string;
+  int: number | PartialNumberValue;
+  float: number | PartialNumberValue;
+};
+
+interface BaseTextFieldProps extends ForwardedInputProps {
+  label: string;
+  name: string;
+  description?: string;
+  orientation?: 'horizontal' | 'vertical';
+}
+
+type TextFieldProps = TextFieldTypeProps & BaseTextFieldProps;
+
+const REMAPPED_TYPES: Partial<Record<TextFieldType, string>> = {
+  int: 'number',
+  float: 'number',
+};
+
+type Parser<Type extends TextFieldType> = (
+  event: ChangeEvent<HTMLInputElement>,
+  props: BaseTextFieldProps & Extract<TextFieldTypeProps, { type?: Type }>
+) => TextFieldValueByType[Type];
+
+type ParsersMap = {
+  [Type in TextFieldType]?: Parser<Type>;
+};
+
+const parsers: ParsersMap = {
+  int: (event) => {
+    const min = event.target.min ? parseFloat(event.target.min) : -Infinity;
+    const max = event.target.max ? parseFloat(event.target.max) : Infinity;
+    const value = parseIntValue(event.target.value);
+
+    return typeof value === 'number' ? clamp(value, min, max) : value;
+  },
+  float: (event, { scale }) => {
+    const min = event.target.min ? parseFloat(event.target.min) : -Infinity;
+    const max = event.target.max ? parseFloat(event.target.max) : Infinity;
+    const value = parseFloatValue(event.target.value, { scale });
+
+    return typeof value === 'number' ? clamp(value, min, max) : value;
+  },
+};
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
+  const {
+    className,
+    description,
+    label,
+    name,
+    type = 'text',
+    orientation = 'vertical',
+    ...rest
+  } = props;
+  const inputType = REMAPPED_TYPES[type] || type;
+  const parser = parsers[type];
+  const [field, meta, { setValue }] = useField({ name, type: inputType });
+
+  return (
+    <div
+      className={cx(className, 'flex flex-1 gap-2', {
+        'flex-col': orientation === 'vertical',
+        'items-center justify-between': orientation === 'horizontal',
+      })}
+    >
+      <div
+        className={cx('flex flex-col gap-1', {
+          'flex-1': orientation === 'horizontal',
+        })}
+      >
+        <label htmlFor={name}>{label}</label>
+        {description && (
+          <span className="text-offwhite max-w-lg text-sm">{description}</span>
+        )}
+      </div>
+      <input
+        {...field}
+        {...omit(rest, 'scale')}
+        ref={ref}
+        type={inputType}
+        value={field.value ?? ''}
+        className={cx(
+          'h-10 rounded bg-[#333] px-4 text-sm text-white disabled:cursor-not-allowed disabled:opacity-50',
+          { 'min-w-[200px]': orientation === 'horizontal' }
+        )}
+        onChange={(event) => {
+          if (parser) {
+            return setValue(parser(event, omit(props, ['type'])));
+          }
+        }}
+      />
+    </div>
+  );
+});
+
+export default TextField;
