a3c1095d25c8d8558195684fcdfbd18d1f346a05
Jerel Miller <jerelmiller@gmail.com>
2023-01-25 15:22:07 -0700

    Add a mutation to reset field config

---
+++ client/src/mutations/resetFieldConfigMutation.ts
@@ -0,0 +1,39 @@
+import { gql, useMutation } from '@apollo/client';
+import { useCallback } from 'react';
+import {
+  ResetFieldConfigInput,
+  ResetFieldConfigMutation,
+  ResetFieldConfigMutationVariables,
+} from '../types/api';
+
+const RESET_FIELD_CONFIG_MUTATION = gql`
+  mutation ResetFieldConfigMutation($input: ResetFieldConfigInput!) {
+    resetFieldConfig(input: $input) {
+      fieldConfig {
+        schemaField {
+          fieldName
+          typename
+        }
+        timeout
+        errorRate
+      }
+    }
+  }
+`;
+
+const resetFieldConfigMutation = () => {
+  const [execute, result] = useMutation<
+    ResetFieldConfigMutation,
+    ResetFieldConfigMutationVariables
+  >(RESET_FIELD_CONFIG_MUTATION);
+
+  const resetFieldConfig = useCallback(
+    (input: ResetFieldConfigInput) => {
+      return execute({ variables: { input } });
+    },
+    [execute]
+  );
+  return [resetFieldConfig, result];
+};
+
+export default resetFieldConfigMutation;
