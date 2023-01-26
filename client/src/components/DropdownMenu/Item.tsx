7134e68b958649c14a000d54d09d6eb159a26970
Jerel Miller <jerelmiller@gmail.com>
2023-01-25 22:10:37 -0700

    Add external link icon to dropdown menu

--- client/src/components/DropdownMenu/Item.tsx
+++ client/src/components/DropdownMenu/Item.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
 import { Link, LinkProps } from 'react-router-dom';
 import * as Dropdown from '@radix-ui/react-dropdown-menu';
 import type { DropdownMenuItemProps } from '@radix-ui/react-dropdown-menu';
 import styles from './Item.module.scss';
+import { ExternalLink } from 'lucide-react';
 
 interface ItemProps {
   children?: ReactNode;
@@ -21,7 +23,16 @@ const Item = ({ children, disabled, onSelect, to }: ItemProps) => {
     <Dropdown.Item asChild disabled={disabled} onSelect={onSelect}>
       <li className={styles.container}>
         {isExternal ? (
-          <a {...props}>{props.children}</a>
+          <a
+            {...props}
+            href={to}
+            target="_blank"
+            rel="noreferrer"
+            className={cx(styles.item, 'flex items-center justify-between')}
+          >
+            {props.children}
+            <ExternalLink size="1.25rem" strokeWidth={1.5} />
+          </a>
         ) : to ? (
           <Link to={to} {...props} />
         ) : (
