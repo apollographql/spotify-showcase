305df0020961f6483bd325004726814250323e62
Jerel Miller <jerelmiller@gmail.com>
2023-01-30 16:20:35 -0700

    Create a hook to make it easy to fetch more results

---
+++ client/src/hooks/usePaginationObserver.ts
@@ -0,0 +1,54 @@
+import { RefObject, useRef } from 'react';
+import useIntersectionObserver from './useIntersectionObserver';
+import useScrollContainer from './useScrollContainer';
+
+interface PageInfo {
+  offset: number;
+  limit: number;
+  hasNextPage: boolean;
+}
+
+interface Options {
+  fetchMore: (options: { variables: { offset: number } }) => Promise<unknown>;
+  pageInfo: PageInfo | undefined;
+  scrollContainer?: Element;
+  threshold?: `${string}px`;
+}
+
+const usePaginationObserver = (
+  ref: RefObject<Element | undefined>,
+  { fetchMore, scrollContainer, pageInfo, threshold = '500px' }: Options
+) => {
+  const isLoadingRef = useRef(false);
+  const defaultScrollContainer = useScrollContainer();
+
+  useIntersectionObserver(
+    ref,
+    {
+      root: defaultScrollContainer || scrollContainer || null,
+      rootMargin: `0px 0px ${threshold} 0px`,
+    },
+    async ([entry]) => {
+      const { isIntersecting } = entry;
+
+      if (
+        isLoadingRef.current ||
+        !isIntersecting ||
+        !pageInfo ||
+        !pageInfo.hasNextPage
+      ) {
+        return;
+      }
+
+      const { offset, limit } = pageInfo;
+
+      isLoadingRef.current = true;
+
+      await fetchMore({ variables: { offset: offset + limit } });
+
+      isLoadingRef.current = false;
+    }
+  );
+};
+
+export default usePaginationObserver;
