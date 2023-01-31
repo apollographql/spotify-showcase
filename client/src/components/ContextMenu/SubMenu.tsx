39e89231e0982740176040ac7c0c6bba5a5d14a0
Jerel Miller <jerelmiller@gmail.com>
2023-01-30 17:57:45 -0700

    Update SubMenu to wrap with ScrollContainerContext

--- client/src/components/ContextMenu/SubMenu.tsx
+++ client/src/components/ContextMenu/SubMenu.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from 'react';
+import { useRef, ReactNode } from 'react';
 import * as ContextMenu from '@radix-ui/react-context-menu';
 import { ChevronRight } from 'lucide-react';
+import ScrollContainerContext from '../ScrollContainerContext';
 
 interface SubMenuProps {
   children: ReactNode;
@@ -8,6 +9,8 @@ interface SubMenuProps {
 }
 
 const SubMenu = ({ children, content }: SubMenuProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <ContextMenu.Sub>
       <ContextMenu.SubTrigger className="data-open:bg-white/10 flex cursor-pointer items-center justify-between gap-1 rounded-sm p-2 hover:bg-white/10 hover:outline-0">
@@ -15,12 +18,15 @@ const SubMenu = ({ children, content }: SubMenuProps) => {
         <ChevronRight size="1em" />
       </ContextMenu.SubTrigger>
       <ContextMenu.Portal>
-        <ContextMenu.SubContent
-          className="bg-surface max-w[350px] z-10 max-h-[calc(100vh-1rem*2)] min-w-[160px] overflow-auto rounded p-1 text-sm text-white shadow-xl"
-          collisionPadding={20}
-        >
-          {content}
-        </ContextMenu.SubContent>
+        <ScrollContainerContext.Provider value={ref}>
+          <ContextMenu.SubContent
+            ref={ref}
+            className="bg-surface max-w[350px] z-10 max-h-[calc(100vh-1rem*2)] min-w-[160px] overflow-auto rounded p-1 text-sm text-white shadow-xl"
+            collisionPadding={20}
+          >
+            {content}
+          </ContextMenu.SubContent>
+        </ScrollContainerContext.Provider>
       </ContextMenu.Portal>
     </ContextMenu.Sub>
   );
