8a59751b8d3b3e28da086784961206b3531f61ab
Jerel Miller <jerelmiller@gmail.com>
2023-01-31 16:47:27 -0700

    Install and configure mdx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  server: {
-    open: true,
-    port: 3000,
-  },
-  plugins: [react()],
+export default defineConfig(async () => {
+  const mdx = await import('@mdx-js/rollup');
+
+  return {
+    server: {
+      open: true,
+      port: 3000,
+    },
+    plugins: [mdx.default(), react()],
+  };
 });
