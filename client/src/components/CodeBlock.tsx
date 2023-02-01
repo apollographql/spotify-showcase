61f55f204c160c107002ed2779e148a139211e81
Jerel Miller <jerelmiller@gmail.com>
2023-02-01 01:55:13 -0700

    Add line numbers to code blocks

--- client/src/components/CodeBlock.tsx
+++ client/src/components/CodeBlock.tsx
@@ -22,13 +22,26 @@ const CodeBlock = ({ language, code }: CodeBlockProps) => {
       language={language as Language}
     >
       {({ className, getLineProps, getTokenProps, tokens }) => (
-        <pre className={cx(className, 'rounded border border-white/5')}>
+        <pre
+          className={cx(
+            className,
+            'max-h-[400px] rounded border border-white/5'
+          )}
+        >
           <code>
             {tokens.map((line, index) => (
-              <div key={index} {...getLineProps({ line, key: index })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
+              <div
+                key={index}
+                {...getLineProps({ line, key: index, className: 'flex gap-4' })}
+              >
+                <div className="w-[2ch] select-none text-right opacity-50">
+                  {index + 1}
+                </div>
+                <div>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
               </div>
             ))}
           </code>
