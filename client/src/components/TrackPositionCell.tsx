1ac4ca9c76a4b4a646ff7b08a037f985b408cb15
Jerel Miller <jerelmiller@gmail.com>
2023-02-20 16:15:48 -0700

    Create a low-level component for the track position

---
+++ client/src/components/TrackPositionCell.tsx
@@ -0,0 +1,39 @@
+import { PauseIcon, PlayIcon } from 'lucide-react';
+import AnimatedSoundWave from './AnimatedSoundWave';
+import Text from './Text';
+
+interface TrackPositionCellProps {
+  playing: boolean;
+  position: number;
+}
+
+const TrackPositionCell = ({ playing, position }: TrackPositionCellProps) => {
+  return (
+    <div className="flex min-w-[3ch] justify-end">
+      {playing ? (
+        <>
+          <AnimatedSoundWave className="group-hover:hidden" />
+          <button className="hidden group-hover:block">
+            <PauseIcon size="1rem" fill="currentColor" />
+          </button>
+        </>
+      ) : (
+        <>
+          <Text
+            className="group-hover:hidden"
+            color={playing ? 'themeLight' : 'muted'}
+            numericVariant="tabular-nums"
+            size="base"
+          >
+            {position}
+          </Text>
+          <button className="hidden group-hover:block">
+            <PlayIcon size="1rem" fill="currentColor" />
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TrackPositionCell;
