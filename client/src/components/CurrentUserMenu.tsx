cf4276f16178212f255c8a3b3556afcc757d19d0
Jerel Miller <jerelmiller@gmail.com>
2023-03-08 14:06:31 -0700

    Add link to provide feedback in dropdown menu

--- client/src/components/CurrentUserMenu.tsx
+++ client/src/components/CurrentUserMenu.tsx
@@ -50,6 +50,9 @@ const CurrentUserMenu = () => {
         <DropdownMenu.Item to="https://github.com/apollographql/spotify-showcase">
           View the source code
         </DropdownMenu.Item>
+        <DropdownMenu.Item to="https://o0urpu09l9p.typeform.com/to/SrKsN0nv">
+          Give us feedback
+        </DropdownMenu.Item>
         <DropdownMenu.Separator />
         <DropdownMenu.Item to="/logout">Logout</DropdownMenu.Item>
       </DropdownMenu.Menu>
