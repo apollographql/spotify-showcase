70f54be4817b6180861a4b0b1e90916160078614
Jerel Miller <jerelmiller@gmail.com>
2023-03-08 18:36:06 -0700

    Move eslint to root

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,21 @@ module.exports = {
     commonjs: true,
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      files: ['client/src/**/*.{ts,tsx}'],
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+    },
   ],
-  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
