810f1524f53936f6c1040db4b88f60885fcbcf6b
Jerel Miller <jerelmiller@gmail.com>
2023-06-17 23:09:53 -0600

    Move the backdrop color into the main layout component

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,10 +1,9 @@
-import { ComponentPropsWithoutRef, CSSProperties, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 import cx from 'classnames';
 
 import Header from './Header';
 import Main from './Main';
 import Sidebar from './Sidebar';
-import useBackgroundColor from '../../hooks/useBackgroundColor';
 
 interface LayoutProps
   extends Omit<ComponentPropsWithoutRef<'div'>, 'className'> {
@@ -12,13 +11,7 @@ interface LayoutProps
   type: 'fullscreen' | 'player' | 'loggedOut';
 }
 
-interface BackdropStyle extends CSSProperties {
-  '--backdrop-color': string;
-}
-
 const Layout = ({ children, type, ...props }: LayoutProps) => {
-  const [backgroundColor] = useBackgroundColor();
-
   return (
     <div
       {...props}
@@ -31,10 +24,6 @@ const Layout = ({ children, type, ...props }: LayoutProps) => {
         '[grid-template-areas:"main-view"]': type === 'fullscreen',
       })}
     >
-      <div
-        className="[background:var(--backdrop-color)] fixed transition duration-200 ease-out inset-0 z-[-1] after:absolute after:inset-0 after:bg-[linear-gradient(rgba(255,255,255,0),#04060b)] after:transition-opacity after:duration-300 after:opacity-100"
-        style={{ '--backdrop-color': backgroundColor } as BackdropStyle}
-      />
       {children}
     </div>
   );
--- client/src/components/Layout/Main.tsx
+++ client/src/components/Layout/Main.tsx
@@ -1,15 +1,26 @@
-import { forwardRef, ReactNode } from 'react';
+import { CSSProperties, forwardRef, ReactNode } from 'react';
+import useBackgroundColor from '../../hooks/useBackgroundColor';
 
 interface MainProps {
   children: ReactNode;
 }
 
+interface BackdropStyle extends CSSProperties {
+  '--backdrop-color': string;
+}
+
 const Main = forwardRef<HTMLElement, MainProps>(({ children }, ref) => {
+  const [backgroundColor] = useBackgroundColor();
+
   return (
     <main
       className="[--main-header--height:80px] [--main-content--padding:2rem] [grid-area:main-view] flex flex-col text-primary h-full overflow-y-auto relative"
       ref={ref}
     >
+      <div
+        className="[background:var(--backdrop-color)] absolute transition duration-200 ease-out inset-0 z-[-1] after:absolute after:inset-0 after:bg-[linear-gradient(rgba(255,255,255,0),#04060b)] after:transition-opacity after:duration-300 after:opacity-100"
+        style={{ '--backdrop-color': backgroundColor } as BackdropStyle}
+      />
       <article className="flex flex-col flex-1">{children}</article>
     </main>
   );
