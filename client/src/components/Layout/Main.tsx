41a6828962e63c003e64a6942d5bf4f30cc89b06
Jerel Miller <jerelmiller@gmail.com>
2023-06-18 00:27:44 -0600

    Adjust how dynamic background is applied

--- client/src/components/Layout/Main.tsx
+++ client/src/components/Layout/Main.tsx
@@ -1,5 +1,4 @@
 import { forwardRef, ReactNode } from 'react';
-import AdaptableBackdrop from '../AdaptableBackdrop';
 
 interface MainProps {
   children: ReactNode;
@@ -11,7 +10,6 @@ const Main = forwardRef<HTMLElement, MainProps>(({ children }, ref) => {
       className="[--main-header--height:80px] [--main-content--padding:2rem] [grid-area:main-view] flex flex-col text-primary h-full overflow-y-auto relative rounded-md overflow-hidden"
       ref={ref}
     >
-      <AdaptableBackdrop />
       <article className="flex flex-col flex-1 overflow-y-auto">
         {children}
       </article>
--- client/src/components/Page/HeaderContainer.tsx
+++ client/src/components/Page/HeaderContainer.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import Flex from '../Flex';
 
 interface HeaderContainerProps {
   children?: ReactNode;
@@ -7,13 +6,10 @@ interface HeaderContainerProps {
 
 const HeaderContainer = ({ children }: HeaderContainerProps) => {
   return (
-    <Flex
-      className="p-[var(--main-content--padding)]"
-      gap="2rem"
-      alignItems="end"
-    >
-      {children}
-    </Flex>
+    <div className="p-[var(--main-content--padding)] relative [background:var(--backdrop-color)] transition duration-200 ease-out">
+      <div className="absolute inset-0 bg-[linear-gradient(transparent_0,rgba(0,0,0,0.5)_100%)]" />
+      <div className="isolate flex gap-8 items-end">{children}</div>
+    </div>
   );
 };
 
