3ad8e04d154f858f93ed5ee2d805173d63ba77a6
Jerel Miller <jerelmiller@gmail.com>
2023-06-17 23:48:33 -0600

    Update gradient for adaptable backdrop

--- client/src/components/AdaptableBackdrop.tsx
+++ client/src/components/AdaptableBackdrop.tsx
@@ -10,7 +10,7 @@ const AdaptableBackdrop = () => {
 
   return (
     <div
-      className="[background:var(--backdrop-color)] absolute transition duration-200 ease-out inset-0 z-[-1] after:absolute after:inset-0 after:bg-[linear-gradient(rgba(255,255,255,0),#04060b)] after:transition-opacity after:duration-300 after:opacity-100"
+      className="[background:var(--backdrop-color)] absolute transition duration-200 ease-out inset-0 z-[-1] after:absolute after:inset-0 after:bg-[linear-gradient(rgba(255,255,255,0),#121212)] after:transition-opacity after:duration-300 after:opacity-100"
       style={{ '--backdrop-color': backgroundColor } as BackdropStyle}
     />
   );
