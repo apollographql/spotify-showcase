209c85914f35500b4a57c27a0e39d445ef37334f
Michael Watson <xambrew@gmail.com>
2023-06-27 16:35:28 -0700

    Update index.ts

--- subgraphs/spotify/src/index.ts
+++ subgraphs/spotify/src/index.ts
@@ -49,7 +49,7 @@ async function main() {
         checkRouterSecret(req.headers["router-authorization"] as string);
 
         const token = req.get("Authentication");
-        console.log(JSON.stringify(req));
+        console.log(JSON.stringify(req.headers));
         return {
           defaultCountryCode,
           dataSources: {
