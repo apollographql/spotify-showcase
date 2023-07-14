eee31912d89068f011bd12bfb48399e657fa9ed0
Jerel Miller <jerelmiller@gmail.com>
2023-07-14 15:51:06 -0600

    Use local storage helpers for persisted query var

--- client/src/vars.ts
+++ client/src/vars.ts
@@ -1,6 +1,7 @@
 import { makeVar } from '@apollo/client';
 import { readToken } from './auth';
 import { STORAGE_KEYS } from './constants';
+import { readFromStorage, writeToStorage } from './utils/storage';
 
 export interface Notification {
   message: string;
@@ -9,9 +10,9 @@ export interface Notification {
 export const isLoggedInVar = makeVar(Boolean(readToken()));
 export const notificationVar = makeVar<Notification | null>(null);
 export const persistedQueryModeVar = makeVar(
-  JSON.parse(localStorage.getItem(STORAGE_KEYS.PERSISTED_QUERY_MODE) ?? 'false')
+  readFromStorage<boolean>(STORAGE_KEYS.PERSISTED_QUERY_MODE) ?? false
 );
 
 persistedQueryModeVar.onNextChange((value) => {
-  localStorage.setItem(STORAGE_KEYS.PERSISTED_QUERY_MODE, value);
+  writeToStorage(STORAGE_KEYS.PERSISTED_QUERY_MODE, value);
 });
