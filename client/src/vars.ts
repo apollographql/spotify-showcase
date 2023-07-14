0c7dfc9234f050e311e1d1977d8fda4332b41af3
Jerel Miller <jerelmiller@gmail.com>
2023-07-14 15:50:19 -0600

    Add local storage helper

--- client/src/vars.ts
+++ client/src/vars.ts
@@ -1,5 +1,6 @@
 import { makeVar } from '@apollo/client';
 import { readToken } from './auth';
+import { STORAGE_KEYS } from './constants';
 
 export interface Notification {
   message: string;
@@ -7,4 +8,10 @@ export interface Notification {
 
 export const isLoggedInVar = makeVar(Boolean(readToken()));
 export const notificationVar = makeVar<Notification | null>(null);
-export const persistedQueryModeVar = makeVar(false);
+export const persistedQueryModeVar = makeVar(
+  JSON.parse(localStorage.getItem(STORAGE_KEYS.PERSISTED_QUERY_MODE) ?? 'false')
+);
+
+persistedQueryModeVar.onNextChange((value) => {
+  localStorage.setItem(STORAGE_KEYS.PERSISTED_QUERY_MODE, value);
+});
