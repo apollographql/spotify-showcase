5b1dd2aa3cb388e34667de7b634adc4a3683f7a7
Jerel Miller <jerelmiller@gmail.com>
2023-07-17 23:49:21 -0600

    Minor refactor on getting spotify player

--- client/src/SpotifyConnect.tsx
+++ client/src/SpotifyConnect.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react';
 import { TypedDocumentNode, gql, useSuspenseQuery } from '@apollo/client';
 import { AuthTokenQuery } from './types/api';
-import useScript from './hooks/useScript';
+import useSpotifyPlayer from './hooks/useSpotifyPlayer';
 
 const AUTH_TOKEN_QUERY: TypedDocumentNode<AuthTokenQuery> = gql`
   query AuthTokenQuery {
@@ -14,45 +13,9 @@ const AUTH_TOKEN_QUERY: TypedDocumentNode<AuthTokenQuery> = gql`
 `;
 
 const SpotifyConnect = () => {
-  useScript('https://sdk.scdn.co/spotify-player.js', { async: true });
-  const [ready, setReady] = useState(false);
+  const { data } = useSuspenseQuery(AUTH_TOKEN_QUERY);
 
-  const {
-    client,
-    data: { me: currentUser },
-  } = useSuspenseQuery(AUTH_TOKEN_QUERY);
-
-  const token = currentUser?.auth.accessToken;
-
-  useEffect(() => {
-    window.onSpotifyWebPlaybackSDKReady = () => setReady(true);
-  }, []);
-
-  useEffect(() => {
-    if (!ready || !token) {
-      return;
-    }
-
-    const player = new window.Spotify.Player({
-      name: 'Apollo Spotify Showcase',
-      getOAuthToken: (cb) => cb(token),
-    });
-
-    player.addListener('ready', () => {
-      const { cache } = client;
-      cache.modify({
-        id: cache.identify({ __typename: 'Player' }),
-        fields: {
-          devices: (_, { DELETE }) => DELETE,
-        },
-      });
-    });
-    player.connect();
-
-    return () => {
-      player.disconnect();
-    };
-  }, [client, ready, token]);
+  useSpotifyPlayer(data.me?.auth.accessToken ?? null);
 
   return null;
 };
---
+++ client/src/hooks/useSpotifyPlayer.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { Spotify } from '../types/SpotifyWebPlaybackSDK';
+import useScript from './useScript';
+
+const useSpotifyPlayer = (token: string | null) => {
+  useScript('https://sdk.scdn.co/spotify-player.js', { async: true });
+
+  const [deviceId, setDeviceId] = useState<string>();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    let player: Spotify.Player;
+
+    const initializePlayer = () => {
+      player = new window.Spotify.Player({
+        name: 'Apollo Spotify Showcase',
+        getOAuthToken: (cb) => cb(token),
+      });
+
+      player.addListener('ready', ({ device_id }) => {
+        setDeviceId(device_id);
+      });
+
+      player.connect();
+    };
+
+    if (window.Spotify) {
+      initializePlayer();
+    } else {
+      window.onSpotifyWebPlaybackSDKReady = initializePlayer;
+    }
+
+    return () => {
+      player?.disconnect();
+    };
+  }, [token]);
+
+  return deviceId;
+};
+
+export default useSpotifyPlayer;
