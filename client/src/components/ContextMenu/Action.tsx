d11a9acd0582633c6d7976ff825b65778c35fcd7
Jerel Miller <jerelmiller@gmail.com>
2023-07-19 16:11:12 -0600

    Allow mouse over events on context menu actions

--- client/src/components/ContextMenu/Action.tsx
+++ client/src/components/ContextMenu/Action.tsx
@@ -1,16 +1,20 @@
-import { ReactNode } from 'react';
+import { ReactNode, MouseEvent } from 'react';
 import Item from './Item';
 
 interface ActionProps {
   disabled?: boolean;
   children?: ReactNode;
   onSelect?: () => void;
+  onMouseOver?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Action = ({ children, disabled, onSelect }: ActionProps) => {
+const Action = ({ children, disabled, onSelect, onMouseOver }: ActionProps) => {
   return (
     <Item disabled={disabled} onSelect={onSelect}>
-      <button className="block w-full appearance-none text-left">
+      <button
+        className="block w-full appearance-none text-left"
+        onMouseOver={onMouseOver}
+      >
         {children}
       </button>
     </Item>
