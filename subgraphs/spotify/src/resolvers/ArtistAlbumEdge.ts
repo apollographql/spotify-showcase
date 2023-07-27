7331552c733a30ba8f5dae4ff78d85edd7e4a8ae
Jerel Miller <jerelmiller@gmail.com>
2023-07-27 10:03:29 -0600

    Remove albumGroup field that is not present in the REST API

--- subgraphs/spotify/src/resolvers/ArtistAlbumEdge.ts
+++ subgraphs/spotify/src/resolvers/ArtistAlbumEdge.ts
@@ -1,9 +1,6 @@
 import { ArtistAlbumEdgeResolvers } from '../__generated__/resolvers-types';
-import { itself, prop } from './helpers';
+import { itself } from './helpers';
 
 export const ArtistAlbumEdge: ArtistAlbumEdgeResolvers = {
-  // TODO: Remove this field since it is not available in the API. Temporarily
-  // point to album type
-  albumGroup: prop('album_type'),
   node: itself(),
 };
