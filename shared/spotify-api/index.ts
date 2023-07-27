c8efba8ac6df345627aa5e76d911c8097cb7b251
Jerel Miller <jerelmiller@gmail.com>
2023-07-27 01:47:57 -0600

    Copy spotify client over to shared package

--- shared/spotify-api/index.ts
+++ shared/spotify-api/index.ts
@@ -2,3 +2,4 @@ export { Spotify } from './src/types';
 export { OAUTH_SCOPES } from './src/constants';
 export { SpotifyDataSource } from './src/dataSource';
 export { MockSpotifyClient } from './src/mockClient';
+export { SpotifyClient } from './src/client';
