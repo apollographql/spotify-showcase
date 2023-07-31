6d4bf4f86617ec6b1418a6da4d9c5ae633709847
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 15:16:11 -0600

    Merge some components together in logged in layout

--- client/src/components/LoggedInLayout.tsx
+++ client/src/components/LoggedInLayout.tsx
@@ -45,31 +45,6 @@ const ROOT_QUERY: TypedDocumentNode<RootQuery, RootQueryVariables> = gql`
 
 const LoggedInLayout = () => {
   const ref = useRef<HTMLElement>(null);
-
-  return (
-    <ScrollContainerContext.Provider value={ref}>
-      <div
-        onContextMenu={(e) => e.preventDefault()}
-        className={
-          'grid gap-2 p-2 h-screen grid-cols-[375px_1fr] [grid-template-areas:"sidebar_main-view""playbar_playbar"] [grid-template-rows:1fr_auto]'
-        }
-      >
-        <Layout.Sidebar>
-          <Playlists />
-        </Layout.Sidebar>
-        <Layout.Main ref={ref}>
-          <Layout.Header />
-          <PlaybackStateSubscriber />
-          <Outlet />
-        </Layout.Main>
-        <Playbar className="[grid-area:playbar]" />
-      </div>
-      <NotificationManager />
-    </ScrollContainerContext.Provider>
-  );
-};
-
-const Playlists = () => {
   const [scrollContainer, setScrollContainerRef] =
     useState<HTMLDivElement | null>(null);
   const { data, fetchMore } = useSuspenseQuery(ROOT_QUERY, {
@@ -83,77 +58,97 @@ const Playlists = () => {
   }
 
   return (
-    <Layout.Sidebar.Section className="flex-1 overflow-hidden flex flex-col pb-0">
-      <header className="px-4 py-2">
-        <h2 className="text-muted flex gap-2 items-center py-2 text-base">
-          <Library /> Your Library
-        </h2>
-      </header>
+    <>
       <div
-        className="overflow-y-auto flex-1 -mx-1 px-3"
-        ref={setScrollContainerRef}
+        onContextMenu={(e) => e.preventDefault()}
+        className={
+          'grid gap-2 p-2 h-screen grid-cols-[375px_1fr] [grid-template-areas:"sidebar_main-view""playbar_playbar"] [grid-template-rows:1fr_auto]'
+        }
       >
-        <PlaylistSidebarLink
-          pinned
-          playlist={{
-            __typename: 'Playlist',
-            id: 'collection:tracks',
-            name: 'Liked Songs',
-            uri: `spotify:user:${currentUser.user.id}:collection`,
-            owner: {
-              __typename: 'User',
-              id: 'spotify',
-              displayName: 'Spotify',
-            },
-          }}
-          coverPhoto={
-            <GradientIcon
-              backgroundColor="linear-gradient(135deg,#450af5,#c4efd9)"
-              lucideIcon={Heart}
-              iconSize="1rem"
-            />
-          }
-          to="/collection/tracks"
-        />
-        <PlaylistSidebarLink
-          pinned
-          playlist={{
-            __typename: 'Playlist',
-            id: 'collection:episodes',
-            name: 'Your Episodes',
-            uri: `spotify:user:${currentUser.user.id}:collection:your-episodes`,
-            owner: {
-              __typename: 'User',
-              id: 'spotify',
-              displayName: 'Spotify',
-            },
-          }}
-          coverPhoto={
-            <GradientIcon
-              fill="var(--color--theme)"
-              backgroundColor="#056952"
-              lucideIcon={Bookmark}
-              iconSize="1rem"
-            />
-          }
-          to="/collection/episodes"
-        />
-        {data.me?.playlists?.edges.map(({ node: playlist }) => (
-          <PlaylistSidebarLink
-            pinned={false}
-            key={playlist.id}
-            playlist={playlist}
-            coverPhoto={<CoverPhoto image={thumbnail(playlist.images)} />}
-            to={`/playlists/${playlist.id}`}
-          />
-        ))}
-        <OffsetBasedPaginationObserver
-          pageInfo={data.me?.playlists?.pageInfo}
-          fetchMore={fetchMore}
-          scrollContainer={scrollContainer}
-        />
+        <Layout.Sidebar>
+          <Layout.Sidebar.Section className="flex-1 overflow-hidden flex flex-col pb-0">
+            <header className="px-4 py-2">
+              <h2 className="text-muted flex gap-2 items-center py-2 text-base">
+                <Library /> Your Library
+              </h2>
+            </header>
+            <div
+              className="overflow-y-auto flex-1 -mx-1 px-3"
+              ref={setScrollContainerRef}
+            >
+              <PlaylistSidebarLink
+                pinned
+                playlist={{
+                  __typename: 'Playlist',
+                  id: 'collection:tracks',
+                  name: 'Liked Songs',
+                  uri: `spotify:user:${currentUser.user.id}:collection`,
+                  owner: {
+                    __typename: 'User',
+                    id: 'spotify',
+                    displayName: 'Spotify',
+                  },
+                }}
+                coverPhoto={
+                  <GradientIcon
+                    backgroundColor="linear-gradient(135deg,#450af5,#c4efd9)"
+                    lucideIcon={Heart}
+                    iconSize="1rem"
+                  />
+                }
+                to="/collection/tracks"
+              />
+              <PlaylistSidebarLink
+                pinned
+                playlist={{
+                  __typename: 'Playlist',
+                  id: 'collection:episodes',
+                  name: 'Your Episodes',
+                  uri: `spotify:user:${currentUser.user.id}:collection:your-episodes`,
+                  owner: {
+                    __typename: 'User',
+                    id: 'spotify',
+                    displayName: 'Spotify',
+                  },
+                }}
+                coverPhoto={
+                  <GradientIcon
+                    fill="var(--color--theme)"
+                    backgroundColor="#056952"
+                    lucideIcon={Bookmark}
+                    iconSize="1rem"
+                  />
+                }
+                to="/collection/episodes"
+              />
+              {data.me?.playlists?.edges.map(({ node: playlist }) => (
+                <PlaylistSidebarLink
+                  pinned={false}
+                  key={playlist.id}
+                  playlist={playlist}
+                  coverPhoto={<CoverPhoto image={thumbnail(playlist.images)} />}
+                  to={`/playlists/${playlist.id}`}
+                />
+              ))}
+              <OffsetBasedPaginationObserver
+                pageInfo={data.me?.playlists?.pageInfo}
+                fetchMore={fetchMore}
+                scrollContainer={scrollContainer}
+              />
+            </div>
+          </Layout.Sidebar.Section>
+        </Layout.Sidebar>
+        <ScrollContainerContext.Provider value={ref}>
+          <Layout.Main ref={ref}>
+            <Layout.Header />
+            <PlaybackStateSubscriber />
+            <Outlet />
+          </Layout.Main>
+        </ScrollContainerContext.Provider>
+        <Playbar className="[grid-area:playbar]" />
       </div>
-    </Layout.Sidebar.Section>
+      <NotificationManager />
+    </>
   );
 };
 
