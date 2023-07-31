42e77e46f5ac665a4a3fe4000e78f4595bd80c98
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 16:35:40 -0600

    Shorten implemenation of checkContainsAlbums

--- shared/spotify-api/src/mockClient.ts
+++ shared/spotify-api/src/mockClient.ts
@@ -179,16 +179,7 @@ export class MockSpotifyClient implements SpotifyDataSource {
   }
 
   async checkContainsAlbums(ids: string): Promise<boolean[]> {
-    const response = [];
-    const splitIds = ids.split(',');
-    const albums = Object.keys(mocks.albums) ?? [];
-
-    for (let i = 0; i < splitIds.length; i++) {
-      if (albums.includes(splitIds[i])) response.push(true);
-      else response.push(false);
-    }
-
-    return response;
+    return ids.split(',').map((id) => Boolean(mocks.albums[id]));
   }
 
   async checkContainsEpisodes(ids: string): Promise<boolean[]> {
