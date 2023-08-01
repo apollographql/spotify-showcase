c5956b65fdaa15bc992ee963b6da1406deb6bf9d
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 21:47:59 -0600

    Add helper to create persisted var. Add new reactive var

--- client/src/vars.ts
+++ client/src/vars.ts
@@ -1,18 +1,36 @@
 import { makeVar } from '@apollo/client';
 import { readToken } from './auth';
 import { STORAGE_KEYS } from './constants';
-import { readFromStorage, writeToStorage } from './utils/storage';
+import { readFromStorage, writeToStorage, StorageKey } from './utils/storage';
 
 export interface Notification {
   message: string;
 }
 
+const makePersistedVar = <TValue>(
+  storageKey: StorageKey,
+  defaultValue: TValue
+) => {
+  const reactiveVar = makeVar(
+    readFromStorage<TValue>(storageKey) ?? defaultValue
+  );
+
+  reactiveVar.onNextChange((value) => {
+    writeToStorage(storageKey, value);
+  });
+
+  return reactiveVar;
+};
+
 export const isLoggedInVar = makeVar(Boolean(readToken()));
 export const notificationVar = makeVar<Notification | null>(null);
-export const persistedQueryModeVar = makeVar(
-  readFromStorage<boolean>(STORAGE_KEYS.PERSISTED_QUERY_MODE) ?? false
+
+export const highlightSuspenseBoundaries = makePersistedVar(
+  STORAGE_KEYS.HIGHLIGHT_SUSPENSE_BOUNDARIES,
+  false
 );
 
-persistedQueryModeVar.onNextChange((value) => {
-  writeToStorage(STORAGE_KEYS.PERSISTED_QUERY_MODE, value);
-});
+export const persistedQueryModeVar = makePersistedVar(
+  STORAGE_KEYS.PERSISTED_QUERY_MODE,
+  false
+);
