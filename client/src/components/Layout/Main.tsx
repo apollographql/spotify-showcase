b201df143080b46cdaa249cda6fd036d58938b99
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 21:31:44 -0600

    Add a background to main area

--- client/src/components/Layout/Main.tsx
+++ client/src/components/Layout/Main.tsx
@@ -9,7 +9,7 @@ const Main = forwardRef<HTMLElement, MainProps>(({ children }) => {
   const scrollRef = useRef<HTMLElement>(null);
 
   return (
-    <main className="[--main-header--height:80px] [--main-content--padding:2rem] [grid-area:main-view] flex flex-col text-primary h-full overflow-y-auto relative rounded-md overflow-hidden">
+    <main className="[--main-header--height:80px] [--main-content--padding:2rem] [grid-area:main-view] flex flex-col text-primary h-full overflow-y-auto relative rounded-md overflow-hidden bg-black-base">
       <article ref={scrollRef} className="flex flex-col flex-1 overflow-y-auto">
         <ScrollContainerContext.Provider value={scrollRef}>
           {children}
