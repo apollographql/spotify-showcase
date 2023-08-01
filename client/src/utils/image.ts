31ae6ce07f6a7a978dac2ed065744a277429d03e
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 22:49:05 -0600

    Add utility to get vibrant color from image

--- client/src/utils/image.ts
+++ client/src/utils/image.ts
@@ -1,3 +1,8 @@
+import Vibrant from 'node-vibrant';
+
+type Palette = Awaited<ReturnType<Vibrant['getPalette']>>;
+type Swatch = Palette[string];
+
 interface Image {
   __typename: 'Image';
 }
@@ -6,4 +11,29 @@ export const thumbnail = <TImage extends Image>(images: TImage[]) => {
   // Images are ordered widest first, so the smallest image should be last in
   // the array
   return images[images.length - 1];
+};
+
+const PREFERRED_SWATCHES = [
+  'Vibrant',
+  'LightVibrant',
+  'DarkVibrant',
+  'Muted',
+  'LightMuted',
+  'DarkMuted',
+];
+
+export const getVibrantColor = async (src: string) => {
+  return new Promise<Swatch | undefined>((resolve) => {
+    const img = new Image();
+
+    img.crossOrigin = 'anonymous';
+    img.onload = async () => {
+      const palette = await Vibrant.from(img).getPalette();
+      const name = PREFERRED_SWATCHES.find((name) => palette[name]);
+
+      resolve(name ? palette[name] : undefined);
+    };
+
+    img.src = src;
+  });
 };
