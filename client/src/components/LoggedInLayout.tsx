2ff69a5b77e1989849a2a02ac13a4e695ab1b1d5
Jerel Miller <jerelmiller@gmail.com>
2023-07-31 20:57:48 -0600

    Add a better playbar loading state

--- client/src/components/LoggedInLayout.tsx
+++ client/src/components/LoggedInLayout.tsx
@@ -5,7 +5,7 @@ import ScrollContainerContext from './ScrollContainerContext';
 import Playbar from './Playbar';
 import PlaybackStateSubscriber from './PlaybackStateSubscriber';
 import { TypedDocumentNode, gql, useSuspenseQuery } from '@apollo/client';
-import { SidebarQuery, SidebarQueryVariables } from '../types/api';
+import { RepeatMode, SidebarQuery, SidebarQueryVariables } from '../types/api';
 import PlaylistSidebarLink from './PlaylistSidebarLink';
 import { Library } from 'lucide-react';
 import CoverPhoto from './CoverPhoto';
@@ -16,6 +16,18 @@ import YourEpisodesPlaylistCoverPhoto from './YourEpisodesPlaylistCoverPhoto';
 import { randomBetween, range } from '../utils/common';
 import Skeleton from './Skeleton';
 import CurrentUserMenu from './CurrentUserMenu';
+import LikeButton from './LikeButton';
+import PlayButton from './PlayButton';
+import ShufflePlaybackControl from './ShufflePlaybackControl';
+import SkipToPreviousControl from './SkipToPreviousControl';
+import SkipToNextControl from './SkipToNextControl';
+import PlaybackItemProgressBar from './PlaybackItemProgressBar';
+import RepeatControl from './RepeatControl';
+import QueueControlButton from './QueueControlButton';
+import PlaybarControlButton from './PlaybarControlButton';
+import DeviceIcon from './DeviceIcon';
+import MuteControl from './MuteControl';
+import VolumeBar from './VolumeBar';
 
 const LoggedInLayout = () => {
   return (
@@ -182,13 +194,13 @@ const LoadingState = () => {
   return (
     <Layout type="player">
       <Layout.Sidebar>
-        <Layout.Sidebar.Section className="flex flex-col flex-1">
+        <Layout.Sidebar.Section className="flex-1 overflow-hidden flex flex-col pb-0">
           <header className="px-4 py-2">
             <h2 className="text-muted flex gap-2 items-center py-2 text-base">
               <Library /> Your Library
             </h2>
           </header>
-          <div className="flex-1">
+          <div className="overflow-y-auto flex-1 -mx-1 px-3">
             {skeletons.map((num) => (
               <li key={num} className="px-0 py-2">
                 <Skeleton.Text key={num} width={`${randomBetween(40, 60)}%`} />
@@ -205,6 +217,44 @@ const LoadingState = () => {
           </div>
         </header>
       </Layout.Main>
+      <footer className="[grid-area:playbar] flex flex-col">
+        <div className="items-center grid grid-cols-[30%_1fr_30%] text-primary py-5 px-6">
+          <div className="flex items-center gap-4">
+            <Skeleton.CoverPhoto size="4rem" />
+            <div className="flex flex-col gap-2">
+              <Skeleton.Text width="4rem" />
+              <Skeleton.Text width="8rem" />
+            </div>
+            <LikeButton disabled liked={false} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center gap-5 justify-center">
+              <ShufflePlaybackControl
+                disallowed
+                size="1.25rem"
+                shuffled={false}
+              />
+              <SkipToPreviousControl disallowed progressMs={0} />
+              <PlayButton disabled playing={false} size="2.5rem" />
+              <SkipToNextControl disallowed />
+              <RepeatControl disallowed repeatState={RepeatMode.Off} />
+            </div>
+            <PlaybackItemProgressBar playbackState={null} />
+          </div>
+          <div className="flex justify-end gap-4 items-center">
+            <QueueControlButton />
+            <PlaybarControlButton
+              disallowed
+              icon={<DeviceIcon device={undefined} strokeWidth={1.5} />}
+              tooltip="Connect to a device"
+            />
+            <div className="flex gap-2 items-center">
+              <MuteControl disallowed volumePercent={100} />
+              <VolumeBar volumePercent={100} width="100px" />
+            </div>
+          </div>
+        </div>
+      </footer>
     </Layout>
   );
 };
