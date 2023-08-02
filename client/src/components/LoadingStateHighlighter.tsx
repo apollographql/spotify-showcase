f79947cad9c3bb03d6d91fb806b1c6a8d323a1ee
Jerel Miller <jerelmiller@gmail.com>
2023-08-02 15:04:18 -0600

    Add a shade to the loading state highlighter to see a background color

--- client/src/components/LoadingStateHighlighter.tsx
+++ client/src/components/LoadingStateHighlighter.tsx
@@ -1,25 +1,41 @@
-import { cloneElement, Fragment, ReactNode } from 'react';
+import { cloneElement, CSSProperties, Fragment, ReactNode } from 'react';
 import { useReactiveVar } from '@apollo/client';
 import { highlightSuspenseBoundariesVar } from '../vars';
 
 interface LoadingStateHighlighterProps {
+  shade?: string;
   children: ReactNode;
+}
+
+interface StyleProps extends CSSProperties {
+  '--shade': string;
 }
 
 const LoadingStateHighlighter = ({
   children,
+  shade,
 }: LoadingStateHighlighterProps) => {
   const highlightSuspenseBoundaries = useReactiveVar(
     highlightSuspenseBoundariesVar
   );
 
-  return cloneElement(
-    highlightSuspenseBoundaries ? (
-      <div className="[&>*]:border-2 [&>*]:border-suspense-boundary" />
-    ) : (
-      <Fragment />
-    ),
-    { children }
+  return highlightSuspenseBoundaries ? (
+    <div className="relative">
+      <div
+        className="border-2 border-[var(--border-color)] absolute inset-0"
+        style={{ '--border-color': shade || 'border-suspense-boundary' }}
+      >
+        {shade && (
+          <div
+            className="absolute inset-0 opacity-25 bg-[var(--shade)]"
+            style={{ '--shade': shade } as StyleProps}
+          />
+        )}
+      </div>
+      {children}
+    </div>
+  ) : (
+    children
   );
 };
 
