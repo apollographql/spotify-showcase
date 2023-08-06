01d94220f46ec5e6da9669aee822c22281cf8765
Jerel Miller <jerelmiller@gmail.com>
2023-08-06 02:00:52 -0600

    Create a standard loading state component

---
+++ client/src/components/StandardLoadingState.tsx
@@ -0,0 +1,38 @@
+import Page from './Page';
+import PlayButton from './PlayButton';
+import Skeleton from './Skeleton';
+
+const StandardLoadingState = () => {
+  return (
+    <Page>
+      <Page.SkeletonHeader />
+      <Page.Content>
+        <Page.ActionsBar>
+          <PlayButton
+            disabled
+            variant="primary"
+            size="3.5rem"
+            playing={false}
+          />
+        </Page.ActionsBar>
+        <Skeleton.Table
+          rows={10}
+          columns={[
+            <Skeleton.Text key="text" />,
+            <div key="header" className="flex gap-2 items-end">
+              <Skeleton.CoverPhoto size="2.5rem" />
+              <div className="flex flex-col flex-1 gap-2">
+                <Skeleton.Text width="25%" fontSize="1rem" />
+                <Skeleton.Text width="20%" fontSize="0.75rem" />
+              </div>
+            </div>,
+            <Skeleton.Text key="text2" />,
+            <Skeleton.Text key="text3" />,
+          ]}
+        />
+      </Page.Content>
+    </Page>
+  );
+};
+
+export default StandardLoadingState;
