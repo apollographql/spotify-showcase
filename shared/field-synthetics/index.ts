4d6bcc93c651356e28689ede6fd8e22c8f59c52e
Jerel Miller <jerelmiller@gmail.com>
2023-08-06 00:58:18 -0600

    Remove pointer to missing file

--- shared/field-synthetics/index.ts
+++ shared/field-synthetics/index.ts
@@ -1,5 +1,4 @@
 import * as fieldConfigs from './src/fieldConfigs';
-export { syntheticsDirective } from './src/directives';
 export { addSyntheticsToSchema } from './src/addSyntheticsToSchema';
 export { resolvers } from './src/resolvers';
 export { FieldConfig } from './src/fieldConfig';
--- shared/field-synthetics/src/directives.ts
+++
@@ -1,6 +0,0 @@
-import { gql } from 'graphql-tag';
-import { readFileSync } from 'node:fs';
-
-export const syntheticsDirective = gql(
-  readFileSync('./schema.graphql', { encoding: 'utf8' })
-);
