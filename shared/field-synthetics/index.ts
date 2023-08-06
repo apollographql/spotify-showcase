bac845d56f07954176763a262245e61c3fca48b8
Jerel Miller <jerelmiller@gmail.com>
2023-08-06 00:18:48 -0600

    Export field configs from shared package

--- shared/field-synthetics/index.ts
+++ shared/field-synthetics/index.ts
@@ -1,2 +1,6 @@
+import * as fieldConfigs from './src/fieldConfigs';
 export { syntheticsDirective } from './src/directives';
 export { addSyntheticsToSchema } from './src/addSyntheticsToSchema';
+export { FieldConfig } from './src/fieldConfig';
+
+export { fieldConfigs };
