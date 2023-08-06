2ebcf1a70f6d79255f68534f6724c89ddeb6d76c
Jerel Miller <jerelmiller@gmail.com>
2023-08-06 00:07:06 -0600

    Move directive definitions to graphql file

--- shared/field-synthetics/src/directives.ts
+++ shared/field-synthetics/src/directives.ts
@@ -1,22 +1,6 @@
 import { gql } from 'graphql-tag';
+import { readFileSync } from 'node:fs';
 
-export const syntheticsDirective = gql`
-  scalar ErrorRate
-
-  directive @synthetics(
-    """
-    The synthetic timeout configured for the field.
-    """
-    timeout: Int
-
-    """
-    The synthetic error rate configured for the field.
-    """
-    errorRate: ErrorRate
-
-    """
-    Enable or disable synthetics for the field.
-    """
-    enabled: Boolean = true
-  ) on FIELD
-`;
+export const syntheticsDirective = gql(
+  readFileSync('./schema.graphql', { encoding: 'utf8' })
+);
