e69e38f41dd0785811c0e727ded5766d66ba8157
Jerel Miller <jerelmiller@gmail.com>
2023-08-06 01:00:41 -0600

    Remove old field configs files in each subgraph

--- subgraphs/playback/src/types/NullifyOptionalProperties.ts
+++
@@ -1,8 +0,0 @@
-import { OptionalKeysOf } from 'type-fest';
-
-export type NullifyOptionalProperties<T extends object> = Omit<
-  T,
-  OptionalKeysOf<T>
-> & {
-  [K in OptionalKeysOf<T>]?: T[K] | null;
-};
