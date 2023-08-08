8b7705ca5b693919ce7b6beb4e8603e74c606cb3
Jerel Miller <jerelmiller@gmail.com>
2023-08-07 22:50:01 -0600

    Fix current user menu with suspense boundary

--- client/src/components/ConferenceTalk/CurrentUserMenu.tsx
+++ client/src/components/ConferenceTalk/CurrentUserMenu.tsx
@@ -1,4 +1,6 @@
 import CoreCurrentUserMenu, { LoadingState } from '../CurrentUserMenu';
+import cx from 'classnames';
+import { withHighlight } from '../LoadingStateHighlighter';
 
 export const CurrentUserMenu = () => {
   return (
@@ -10,10 +12,25 @@ export const CurrentUserMenu = () => {
   );
 };
 
-CurrentUserMenu.LoadingState = () => {
-  return (
-    <header className="flex items-center justify-end text-primary bg-transparent pt-[var(--main-content--padding)] px-[var(--main-content--padding)] absolute top-0 w-full pointer-events-none flex-shrink-0 z-10">
-      <LoadingState />
-    </header>
-  );
-};
+CurrentUserMenu.LoadingState = withHighlight(
+  ({ isActiveSuspenseBoundary }) => {
+    return (
+      <header
+        className={cx(
+          'flex items-center text-primary bg-transparent pointer-events-none flex-shrink-0 z-10',
+          {
+            ['absolute top-[var(--main-content--padding)] right-[var(--main-content--padding)]']:
+              !isActiveSuspenseBoundary,
+          }
+        )}
+      >
+        <LoadingState />
+      </header>
+    );
+  },
+  {
+    className:
+      '!absolute top-[var(--main-content--padding)] right-[var(--main-content--padding)] z-10',
+    shade: '#00F900',
+  }
+);
