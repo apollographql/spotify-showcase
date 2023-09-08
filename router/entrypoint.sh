09785297fc03b1205d19bf529201001858b1aa9f
o0Ignition0o <jeremy.lempereur@gmail.com>
2023-09-08 21:13:19 +0200

    we re all good

--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -3,7 +3,7 @@ printenv
 
 # wait until both subgraphs are available
 ./wait-for-it.sh -h playback -p 4002
-./wait-for-it.sh -h spotify -p 4002
+./wait-for-it.sh -h spotify -p 4001
 
 echo starting router
 
