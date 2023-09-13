a30c08b73514fe0d242a9acc00b30231b9c722a2
Michael Watson <xambrew@gmail.com>
2023-09-13 16:39:29 -0700

    Standardize on one router yaml
    Make wait-for only run with docker compose through env variable

--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-printenv
 
 # wait until both subgraphs are available
-./wait-for-it.sh -h playback -p 4002
-./wait-for-it.sh -h spotify -p 4001
+case "$WAIT" in
+ true) 
+    ./wait-for-it.sh -h playback -p 4002
+    ./wait-for-it.sh -h spotify -p 4001 ;;
+esac
 
-echo starting router
+echo starting router 
 
 ./router -c router.yaml
