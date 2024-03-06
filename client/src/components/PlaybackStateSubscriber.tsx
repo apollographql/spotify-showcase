3f5ceb7061304c1106eb578015316c6b64100668
Jerel Miller <jerelmiller@gmail.com>
2024-03-05 20:58:52 -0700

    Switch to useSubscription for PlaybackStateSubscriber

--- client/src/components/PlaybackStateSubscriber.tsx
+++ client/src/components/PlaybackStateSubscriber.tsx
@@ -1,12 +1,4 @@
-import { useEffect } from 'react';
-import { ApolloError, gql, useSuspenseQuery } from '@apollo/client';
-import {
-  PlaybackStateSubscriberQuery,
-  PlaybackStateSubscriberSubscription,
-} from '../types/api';
-import merge from 'deepmerge';
-import { overwriteMerge } from '../utils/deepmerge';
-import { useNavigate } from 'react-router-dom';
+import { gql, useSubscription } from '@apollo/client';
 
 const PLAYBACK_STATE_FRAGMENT = gql`
   fragment PlaybackStateFragment on PlaybackState {
@@ -51,20 +43,6 @@ const PLAYBACK_STATE_FRAGMENT = gql`
   }
 `;
 
-const PLAYBACK_STATE_SUBSCRIBER_QUERY = gql`
-  query PlaybackStateSubscriberQuery {
-    me {
-      player {
-        playbackState {
-          ...PlaybackStateFragment
-        }
-      }
-    }
-  }
-
-  ${PLAYBACK_STATE_FRAGMENT}
-`;
-
 const PLAYBACK_STATE_SUBSCRIBER_SUBSCRIPTION = gql`
   subscription PlaybackStateSubscriberSubscription {
     playbackStateChanged {
@@ -76,48 +54,7 @@ const PLAYBACK_STATE_SUBSCRIBER_SUBSCRIPTION = gql`
 `;
 
 const PlaybackStateSubscriber = () => {
-  const { subscribeToMore } = useSuspenseQuery<PlaybackStateSubscriberQuery>(
-    PLAYBACK_STATE_SUBSCRIBER_QUERY
-  );
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    return subscribeToMore<PlaybackStateSubscriberSubscription>({
-      document: PLAYBACK_STATE_SUBSCRIBER_SUBSCRIPTION,
-      onError: (error) => {
-        if (
-          error instanceof ApolloError &&
-          error.graphQLErrors.some(
-            (error) => error.extensions.code === 'UNAUTHENTICATED'
-          )
-        ) {
-          navigate('/logged-out');
-        }
-      },
-      updateQuery: (prev, { subscriptionData }) => {
-        const { playbackStateChanged } = subscriptionData.data;
-
-        const playbackState = playbackStateChanged
-          ? merge(prev.me?.player.playbackState ?? {}, playbackStateChanged, {
-              arrayMerge: overwriteMerge,
-            })
-          : null;
-
-        return {
-          me: {
-            __typename: 'CurrentUser',
-            player: {
-              __typename: 'Player',
-              playbackState,
-            },
-          },
-        };
-      },
-    });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  useSubscription(PLAYBACK_STATE_SUBSCRIBER_SUBSCRIPTION);
 
   return null;
 };
