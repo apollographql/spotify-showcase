787a1911689f3321418e166be70b1d6282b0d047
Jerel Miller <jerelmiller@gmail.com>
2024-03-05 18:38:06 -0700

    Fix issue with build due to mismatch in ts types

--- client/src/components/EpisodePlaybackDetails.tsx
+++ client/src/components/EpisodePlaybackDetails.tsx
@@ -23,6 +23,7 @@ const EpisodePlaybackDetails = ({ episode }: EpisodePlaybackDetailsProps) => {
 
 fragmentRegistry.register(gql`
   fragment EpisodePlaybackDetails_episode on Episode {
+    id
     name
     show {
       id
