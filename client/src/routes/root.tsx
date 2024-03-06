4b9179037cff9453efc50cfb68b0ab85225c2eb2
Jerel Miller <jerelmiller@gmail.com>
2024-03-05 20:30:16 -0700

    Add suspense boundary around logged in layout

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -2,13 +2,20 @@ import useIsLoggedIn from '../hooks/useIsLoggedIn';
 import LoggedOutLayout from '../components/LoggedOutLayout';
 import LoggedInLayout from '../components/LoggedInLayout';
 import NotificationManager from '../components/NotificationManager';
+import { Suspense } from 'react';
 
 export const RouteComponent = () => {
   const isLoggedIn = useIsLoggedIn();
 
   return (
     <>
-      {isLoggedIn ? <LoggedInLayout /> : <LoggedOutLayout />}
+      {isLoggedIn ? (
+        <Suspense fallback={<LoggedInLayout.LoadingState />}>
+          <LoggedInLayout />
+        </Suspense>
+      ) : (
+        <LoggedOutLayout />
+      )}
       <NotificationManager />
     </>
   );
