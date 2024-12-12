53b80635fba9736cb5cd635c189133c16622f178
Jerel Miller <jerelmiller@gmail.com>
2024-12-11 18:11:21 -0700

    Enable globals

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     browser: {
       enabled: true,
       provider: 'playwright',
