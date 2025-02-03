5e3e734a84024ee4b6f01c02bd771e3a0ac62aa0
Jerel Miller <jerelmiller@gmail.com>
2025-02-03 14:50:21 -0700

    Warn on unsafe calls for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -87,6 +87,12 @@ export default ts.config(
     ],
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unsafe-type-assertion': 'warn',
     },
   },
   {
