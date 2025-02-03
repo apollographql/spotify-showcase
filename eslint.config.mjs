d9fbdfde482c6de0746961c8ec6fa7ccd120ea40
Jerel Miller <jerelmiller@gmail.com>
2025-02-03 13:30:29 -0700

    Switch to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,91 @@
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import graphql from '@graphql-eslint/eslint-plugin';
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  {
+    ignores: ['**/dist', '**/__generated__'],
+  },
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['client/src/**/*.{ts,tsx}'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+  },
+  {
+    files: ['client/src/**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }],
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['client/src/**/*.{ts,tsx}'],
+    processor: graphql.processor,
+  },
+  {
+    files: ['client/src/**/*.graphql'],
+    ignores: ['client/src/apollo/localSchema.graphql'],
+    languageOptions: {
+      parser: graphql.parser,
+    },
+    plugins: {
+      '@graphql-eslint': graphql,
+    },
+    ...graphql.configs['flat/operations-recommended'],
+  },
+  {
+    files: ['client/src/**/*.graphql'],
+    ignores: ['client/src/apollo/localSchema.graphql'],
+    rules: {
+      '@graphql-eslint/known-directives': [
+        'error',
+        { ignoreClientDirectives: ['client', 'connection', 'synthetics'] },
+      ],
+      '@graphql-eslint/naming-convention': [
+        'error',
+        {
+          FragmentDefinition: {
+            ignorePattern: '.+_.+',
+          },
+        },
+      ],
+      '@graphql-eslint/no-deprecated': 'warn',
+      '@graphql-eslint/no-unused-fragments': 'off',
+    },
+  }
+);
+
