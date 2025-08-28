be6d376cdde6f78790c2e15be1e8b7e6327239bd
Jerel Miller <jerelmiller@gmail.com>
2025-08-28 17:13:29 -0600

    Fix DefaultContext types

--- client/src/@types/@apollo/client.d.ts
+++ client/src/@types/@apollo/client.d.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import '@apollo/client';
 import { Defer20220824Handler } from '@apollo/client/incremental';
 import { HttpLink } from '@apollo/client';
 
 declare module '@apollo/client' {
-  export interface TypeOverrides
-    extends Defer20220824Handler.TypeOverrides,
-      HttpLink.ContextOptions {}
+  export interface TypeOverrides extends Defer20220824Handler.TypeOverrides {}
+
+  export interface DefaultContext extends HttpLink.ContextOptions {}
 }
